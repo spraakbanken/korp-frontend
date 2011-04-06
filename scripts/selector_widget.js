7a56c4386022ec5f10e01ebfcbd5a2fa566ecd0b
jonatan <jonatan@ef392f78-ee14-0410-84e5-9db46baae5bb>
2011-04-06 15:32:52 +0000

    corpus chooser update
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@30686 ef392f78-ee14-0410-84e5-9db46baae5bb

--- scripts/selector_widget.js
+++ scripts/selector_widget.js
@@ -24,6 +24,8 @@ var hp_corpusChooser = {
 			);
 			
 			
+			
+			
 	},
 	isSelected: function(id) {
 		// Test if a given id is selected
@@ -34,14 +36,76 @@ var hp_corpusChooser = {
 		// Return all ids that are selected
 		var IDArray = new Array();
 		var allboxes = $(".boxdiv label .checked");
-		allboxes.each(function(){
+		allboxes.each(function() {
 			var idstring = $(this).attr('id');
 			IDArray.push(idstring.slice(9));
 		});
 		return IDArray;
 	},
+	selectItems: function(item_ids) {
+		item_ids = $.map(item_ids, function(item){ return "hpcorpus_"+item; })
+		// Check items from outside
+		var allboxes = $(".checkbox");
+		allboxes.each(function() {
+			/* First clear all items */
+			hp_this.setStatus($(this),"unchecked");	
+		});
+		var realboxes = $(".boxdiv label .checkbox");
+		realboxes.each(function() {
+			if($.inArray($(this).attr('id'), item_ids) != -1) {
+				/* Change status of item */
+	 			hp_this.setStatus($(this),"checked");
+	 			hp_this.updateState($(this));
+	 			var ancestors = $(this).parents('.tree');
+	 			ancestors.each(function(){
+		 			hp_this.updateState($(this));
+		 		});
+			}
+		});
+	},
+	updateState: function (element) {
+			// element is a div!
+			var descendants = element.find('.checkbox');
+			var numbOfChecked = 0;
+			var numbOfUnchecked = 0;
+			descendants.each(function(){
+				if (! $(this).parent().parent().hasClass("tree") ) {
+					if( $(this).hasClass("checked") ) {
+						numbOfChecked++;
+					} else if( $(this).hasClass("unchecked") ) {
+						numbOfUnchecked++;
+					}
+				}	
+			});
+			var theBox = element.children('label').children('.checkbox');
+			if (numbOfUnchecked > 0 && numbOfChecked > 0) {
+				this.setStatus(theBox,"intermediate"); // Intermediate
+			} else if (numbOfUnchecked > 0 && numbOfChecked == 0) {
+				this.setStatus(theBox,"unchecked"); // Unchecked
+			} else if (numbOfChecked > 0 && numbOfUnchecked == 0) {
+				this.setStatus(theBox,"checked"); // Checked
+			} 
+		
+	},
+ 	setStatus: function (obj, stat) { /* Change status of item */
+ 			obj.removeClass("intermediate");
+ 			obj.removeClass("unchecked");
+ 			obj.removeClass("checked");
+ 			if(stat == "checked") {
+ 				obj.addClass("checked");
+ 				obj.attr({src : "img/checked.png"});
+ 			} else if(stat == "intermediate") {
+ 				obj.addClass("intermediate");
+ 				obj.attr({src : "img/intermediate.png"});
+ 			} else {
+ 				obj.addClass("unchecked");	
+ 				obj.attr({src : "img/unchecked.png"});
+ 			}
+ 	},
+	
 	_transform: function() {	
 			var el = this.element;
+			hp_this = this;
 			var body;
 			if(this.options.template != '') {
 				body = this.options.template;
@@ -51,7 +115,7 @@ var hp_corpusChooser = {
 			var newHTML = '<div class="scroll_checkboxes">';
 			newHTML += '<div class="hp_topframe buttonlink ui-state-default ui-corner-all"><div style="float:left; padding-top:2px"><span id="hp_corpora_title1"></span><span id="hp_corpora_title2" rel="localize[corpselector_allselected]"></span></div><div style="float:right; width:16px"><span style="text-align:right; left:auto" class="ui-icon ui-icon-triangle-2-n-s"></span></div></div></div>';
 			
-			newHTML += '<div class="popupchecks ui-corner-bottom">'
+			newHTML += '<div class="popupchecks ui-corner-bottom">';
 			
 			newHTML += '<p style="text-align:right; margin-top:10px; margin-right:8px"><a href="#" class="buttonlink ui-state-default ui-corner-all selectall"><span class="ui-icon ui-icon-check"></span> <span rel="localize[corpselector_buttonselectall]">' + this.options.buttonSelectAll + '</span></a> <a href="#" class="selectnone buttonlink ui-state-default ui-corner-all"><span class="ui-icon ui-icon-closethick"></span> <span rel="localize[corpselector_buttonselectnone]">' + this.options.buttonSelectNone + '</span></a></p>';
 			
@@ -101,9 +165,9 @@ var hp_corpusChooser = {
 				var roots = $(this).parent().siblings();
 				roots.each(function() {
 					var check = $(this).children("label").children('.checkbox');
-					setStatus(check,"checked");
+					hp_this.setStatus(check,"checked");
 					$(this).find('.checkbox').each(function() {
-						setStatus($(this),"checked");
+						hp_this.setStatus($(this),"checked");
 					});
 					
 				});
@@ -116,9 +180,9 @@ var hp_corpusChooser = {
 				var roots = $(this).parent().siblings();
 				roots.each(function() {
 					var check = $(this).children("label").children('.checkbox');
-					setStatus(check,"unchecked");
+					hp_this.setStatus(check,"unchecked");
 					$(this).find('.checkbox').each(function() {
-						setStatus($(this),"unchecked");
+						hp_this.setStatus($(this),"unchecked");
 					});
 					
 				});
@@ -142,25 +206,25 @@ var hp_corpusChooser = {
 			
 			$(".boxlabel").unbind("click"); // "folders"
 			$(".boxlabel").click(function() {
-				updateState($(this).parent());
+				hp_this.updateState($(this).parent());
 	 			var childMan = $(this).children('.checkbox');
 	 			if ( childMan.hasClass("checked") ) { // Checked, uncheck it if not the root of a tree
 	 				if (!($(this).parent().hasClass('tree'))) {
-	 					setStatus(childMan,"unchecked");
+	 					hp_this.setStatus(childMan,"unchecked");
 	 				} else {
 	 					var descendants = childMan.parent().siblings('div').find('.checkbox');
-		 				setStatus(descendants,"unchecked");
+		 				hp_this.setStatus(descendants,"unchecked");
 	 				}
 	 			} else { // Unchecked, check it!
-	 				setStatus(childMan,"checked");
+	 				hp_this.setStatus(childMan,"checked");
 	 				if (($(this).parent().hasClass('tree'))) { // If tree, check all descendants
 		 				var descendants = childMan.parent().siblings('div').find('.checkbox');
-		 				setStatus(descendants,"checked");
+		 				hp_this.setStatus(descendants,"checked");
 	 				}
 				}
 				var ancestors = childMan.parents('.tree');
 		 		ancestors.each(function(){
-		 			updateState($(this));
+		 			hp_this.updateState($(this));
 		 		});
 		 		countSelected();
  			});
@@ -168,36 +232,21 @@ var hp_corpusChooser = {
  			$(".boxdiv").unbind("click"); // "Non-folder items"
 			$(".boxdiv").click(function() {
 				$(this).disableSelection();
-				updateState($(this));
+				hp_this.updateState($(this));
 	 			var childMan = $(this).children('label').children('.checkbox');
 				if ( childMan.hasClass("checked") ) {
-					setStatus(childMan,"unchecked");
+					hp_this.setStatus(childMan,"unchecked");
 				} else {
-					setStatus(childMan,"checked");
+					hp_this.setStatus(childMan,"checked");
 				}			
 				var ancestors = childMan.parents('.tree');
 		 		ancestors.each(function(){
-		 			updateState($(this));
+		 			hp_this.updateState($(this));
 		 		});
 		 		countSelected();
  			});
  			
- 		/* Change status of item */
- 		function setStatus(obj, stat) {
- 			obj.removeClass("intermediate");
- 			obj.removeClass("unchecked");
- 			obj.removeClass("checked");
- 			if(stat == "checked") {
- 				obj.addClass("checked");
- 				obj.attr({src : "img/checked.png"});
- 			} else if(stat == "intermediate") {
- 				obj.addClass("intermediate");
- 				obj.attr({src : "img/intermediate.png"});
- 			} else {
- 				obj.addClass("unchecked");	
- 				obj.attr({src : "img/unchecked.png"});
- 			}
- 		}
+
 		
 		/* Update header */
 		function countSelected() {
@@ -228,29 +277,7 @@ var hp_corpusChooser = {
 			$("#hp_corpora_title2").text(util.getLocaleString(header_text_2));
 		}
 		
-		function updateState(element) {
-			// element is a div!
-			var descendants = element.find('.checkbox');
-			var numbOfChecked = 0;
-			var numbOfUnchecked = 0;
-			descendants.each(function(){
-				if (! $(this).parent().parent().hasClass("tree") ) {
-					if( $(this).hasClass("checked") ) {
-						numbOfChecked++;
-					} else if( $(this).hasClass("unchecked") ) {
-						numbOfUnchecked++;
-					}
-				}	
-			});
-			var theBox = element.children('label').children('.checkbox');
-			if (numbOfUnchecked > 0 && numbOfChecked > 0) {
-				setStatus(theBox,"intermediate"); // Intermediate
-			} else if (numbOfUnchecked > 0 && numbOfChecked == 0) {
-				setStatus(theBox,"unchecked"); // Unchecked
-			} else if (numbOfChecked > 0 && numbOfUnchecked == 0) {
-				setStatus(theBox,"checked"); // Checked
-			} 
-		}
+
 		
 		
 		function recursive_transform(einHTML, levelindent) {
