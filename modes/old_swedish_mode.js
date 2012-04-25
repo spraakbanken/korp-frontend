7e43e94844ddcf09ab75b5ad3baab3025f2032ec
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2012-04-25 12:34:14 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@51840 ef392f78-ee14-0410-84e5-9db46baae5bb

---
+++ modes/old_swedish_mode.js
@@ -0,0 +1,27 @@
+
+settings.autocomplete = false;
+settings.lemgramSelect = false;
+settings.wordpicture = false;
+
+$("#lemgram_list_item").remove();
+$("#results-lemgram").remove();
+$("#showLineDiagram").remove();
+
+
+settings.corpora = {};
+settings.corporafolders = {};
+settings.corpora.fornsvenska = {
+	title : "Fornsvenska",
+	description : "Fornsvenska texter",
+               languages : {
+		FORNSVENSKA : "svenska"
+	},
+	within : within.defaultStruct,
+	attributes : {},
+	struct_attributes : {
+		"text_title" : {label : "title"},
+		"text_datefrom" : {label : "year_from"},
+		"text_dateto" : {label : "year_to"},
+	}
+};
+
