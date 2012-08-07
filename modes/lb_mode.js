a88f1882dd8b414ae7234c65fc0ed8adc701c10f
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2012-08-07 09:01:53 +0000

    +lb
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@56327 ef392f78-ee14-0410-84e5-9db46baae5bb

---
+++ modes/lb_mode.js
@@ -0,0 +1,37 @@
+
+settings.primaryColor = "#eecccc";
+settings.primaryLight = "#eee2e2";
+settings.autocomplete = true;
+settings.lemgramSelect = true;
+settings.wordpicture = true;
+
+settings.corpora = {};
+settings.corporafolders = {};
+settings.corpora.lb = {
+	title : "Litteraturbanken",
+	description : '',
+    languages : {
+		LB : "svenska"
+	},
+	within : within.defaultStruct,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix
+	},
+	struct_attributes : {
+		"text_title" : {label : "title"},
+		"text_author" : {label : "author"},
+		"text_url" : {label : "verk", type : "url"},
+		"page_n" : {label : "page"},
+		"page_url" : {label : "page", type : "url"}
+	}
+};
+
