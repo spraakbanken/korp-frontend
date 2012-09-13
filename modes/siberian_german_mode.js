7b0437266c20156db176d7b5fe5a8762586f11c3
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2012-09-13 11:37:41 +0000

    explanation text for siberian
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@58273 ef392f78-ee14-0410-84e5-9db46baae5bb

--- modes/siberian_german_mode.js
+++ modes/siberian_german_mode.js
@@ -13,7 +13,7 @@ settings.corporafolders = {};
 settings.corpora.siberiangermandialogs = {
 	id : "siberiangermandialogs",
 	title : "Sibirientyska",
-	description : "",
+	description : "Sibirientyska är nedtecknad talad tyska som talas idag av c:a 36 000 människor i regionen Krasnojarsk i Sibirien (Ryssland). Texten har c:a 34 000 ord. Alla ryska ord och verbformer har annoterats (ryska ord står i parentes; böjda verbformer – FINIT/INFIN). Databasen befinner sig i testfasen och kommer att justeras i framtiden.<br/>Sibirientyska ingår i ett <a href=\"http://www.sprak.gu.se/kontakta-oss/larare/andersen-christiane/syntax-in-contact/\">samarbetsprojekt</a> mellan Göteborgs universitet och Astafjev universitet i Krasnojarsk.",
 	languages : {
 		siberiangermandialogs : "svenska"
 	},
