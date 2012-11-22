6617a4ea58007fba11471458155f470c19c07ba2
malin <malin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2012-11-22 09:40:17 +0000

    digidaily med nya delkorpusar
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@62882 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/digidaily_mode.js
+++ app/modes/digidaily_mode.js
@@ -4,55 +4,173 @@ settings.primaryLight = "#F2FFFF";
 //settings.autocomplete = false;
 settings.lemgramSelect = false;
 settings.wordpicture = false;
+settings.digidailyattributes = {
+	lemma : attrs.baseform,
+	pos : attrs.pos,
+	lex : attrs.lemgram,
+	dephead : attrs.dephead,
+	deprel : attrs.deprel,
+	ref : attrs.ref,
+	saldo : attrs.saldo,
+	prefix : attrs.prefix,
+	suffix : attrs.suffix
+};
+settings.digidailystruct_attributes = {
+	text_title : {
+		label : "title",
+		displayType : "select",
+		localize : false,
+ 		opts : settings.liteOptions
+	},
+	text_date : {label : "date"}
+};
 
-
+digidailydescription = '<a href="http://digidaily.kb.se/">Digidaily</a> är ett utvecklingsprojekt där Riksarkivet, Kungliga biblioteket och Mittuniversitetet tillsammans ska utveckla rationella metoder och processer för digitalisering av dagstidningar.'
+	
 $("#lemgram_list_item").remove();
 $("#results-lemgram").remove();
 $("#showLineDiagram").remove();
 
+
 settings.corpora = {};
 settings.corporafolders = {};
-settings.corpora["digidaily"] = {
-    morf : 'saldom|dalinm|swedbergm',
-	id : "digidaily",
-	title : "Digidaily",
-	description : "",
-	within : settings.defaultWithin,
-	context : settings.spContext,
-	attributes : {
-		lemma : attrs.baseform,
-		pos : attrs.pos,
-		lex : attrs.lemgram,
-		dephead : attrs.dephead,
-		deprel : attrs.deprel,
-		ref : attrs.ref,
-		saldo : attrs.saldo,
-		prefix : attrs.prefix,
-		suffix : attrs.suffix
-	},
-	struct_attributes : {
-		text_title : {
-			label : "title",
-			displayType : "select",
-			localize : false,
-			dataset : [
-				"Dalpilen",
-				"Gotlands tidning",
- 				"Faluposten",
- 				"Fahlu weckoblad",
- 				"Folkets röst",
- 				"Bollnäs tidning",
-			        ],
-            opts : settings.liteOptions
 
-		},
+settings.corpora["digidaily-dalpilen"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-dalpilen",
+	title : "Dalpilen",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+settings.corpora["digidaily-goteborgsweckoblad"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-goteborgsweckoblad",
+	title : "Göteborgs weckoblad",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
 	
-		text_date : {label : "date"}
-	//	text_edition : {label : "edition"}
-	}
+settings.corpora["digidaily-gotlandstidning"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-gotlandstidning",
+	title : "Gotlands tidning",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
 };
 
 
+settings.corpora["digidaily-faluposten"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-faluposten",
+	title : "Faluposten",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+settings.corpora["digidaily-fahluweckoblad"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-fahluweckoblad",
+	title : "Fahlu weckoblad",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+settings.corpora["digidaily-jonkopingsbladet"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-jonkopingsbladet",
+	title : "Jönköpingsbladet",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+
+/*
+settings.corpora["digidaily-folketsrost"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-folketsrost",
+	title : "Folkets röst",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+settings.corpora["digidaily-bollnastidning"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-bollnastidning",
+	title : "Bollnäs tidning",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+		
+
+settings.corpora["digidaily-norraskane"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-norraskane",
+	title : "Norra Skåne",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+settings.corpora["digidaily-lindesbergsallehanda"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-lindesbergsallehanda",
+	title : "Lindesbergs allehanda",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+
+settings.corpora["digidaily-blekingsposten"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "digidaily-blekingsposten",
+	title : "Blekingsposten",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : settings.digidailyattributes,
+       
+	struct_attributes : settings.digidailystruct_attributes,
+};
+*/
 
 settings.corpusListing = new CorpusListing(settings.corpora);
 
