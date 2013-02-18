0180ce84e00fd801365854710b2d234086278295
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2013-02-18 09:57:08 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@67827 ef392f78-ee14-0410-84e5-9db46baae5bb

---
+++ app/modes/dylan_mode.js
@@ -0,0 +1,31 @@
+settings.primaryColor = "#FFE7D2";
+settings.primaryLight = "#FFF4EB";
+settings.autocomplete = false;
+settings.lemgramSelect = false;
+settings.wordpicture = false;
+
+$("#lemgram_list_item").remove();
+$("#results-lemgram").remove();
+//$("#showLineDiagram").remove();
+
+settings.corpora = {};
+settings.corporafolders = {};
+settings.corpora.dylan = {
+	id : "dylan",
+	title : "Bob Dylan",
+	description : "",
+	//limited_access = true,
+	within : settings.spWithin,
+	context : settings.spContext,
+	attributes : {
+	},
+	struct_attributes : {
+		"album_name" : {label : "album"},
+		"album_year" : {label : "album_year"},
+		"song_title" : {label : "song_title"},
+		"song_year" : {label : "song_year"},
+		"verse_nr" : {label : "verse"}
+	},
+};
+
+settings.corpusListing = new CorpusListing(settings.corpora);
