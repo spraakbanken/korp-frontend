2158affd83d5d5ae6ce8421e48b2878217e47db5
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2013-06-24 11:32:55 +0000

    small test change
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@74596 ef392f78-ee14-0410-84e5-9db46baae5bb

--- test/spec/corpuslisting.js
+++ test/spec/corpuslisting.js
@@ -2,7 +2,6 @@
   describe("config file", function() {
     it("all corpora definitions have the all the required fields", function() {
       var has_all, required_config_fields;
-
       required_config_fields = ["within", "context", "attributes", "struct_attributes", "id", "title"];
       has_all = _(settings.corpora).values().map(function(corp) {
         return _.values(_.pick(corp, required_config_fields)).length === required_config_fields.length;
@@ -11,7 +10,6 @@
     });
     return it("has 'context' in all corpora definitions", function() {
       var within;
-
       within = _(settings.corpora).values().map(function(item) {
         return "within" in item;
       }).all();
@@ -21,7 +19,6 @@
 
   describe("settings.corpusListing", function() {
     var cl;
-
     cl = settings.corpusListing;
     it('has the same number of corpora as the config', function() {
       return expect(cl.corpora.length).toEqual(_.keys(settings.corpora).length);
@@ -31,7 +28,6 @@
     });
     return it('gives a common attribute from vivill and gp2012', function() {
       var attrs;
-
       attrs = cl.subsetFactory(["romi", "romii"]).getStructAttrsIntersection();
       return expect("text_title" in attrs && "text_author" in attrs).toBe(true);
     });
