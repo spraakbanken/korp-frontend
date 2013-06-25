18be6ad28d1313738a81cff75c40a4f932eddff9
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2013-06-25 14:02:11 +0000

    +twitter-pldebatt
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@74644 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/config.js
+++ app/config.js
@@ -3739,6 +3739,54 @@ settings.corpora.twitter = {
 	}
 };
 
+settings.corpora["twitter-pldebatt"] = {
+	id : "twitter-pldebatt",
+	title : "Twitter - Partiledardebatt juni 2013",
+	description : "Material från Twitter, insamlat under partiledardebatten 12 juni 2013 samt några dagar före och efter.",
+	within : {
+		"sentence" : "sentence",
+		"text" : "text"
+	},
+	context : {
+		"1 sentence" : "1 sentence",
+		"1 text" : "1 text"
+	},
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix
+	},
+	struct_attributes : {
+		user_username : {label : "username2"},
+		user_name : {label : "name"},
+		text_datetime : {label : "date"},
+		text_weekday : {label : "weekday"},
+		text_hashtags : {label : "hashtags", type : "set"},
+		text_mentions : {label : "mentions", type : "set"},
+		text_retweets : {label : "retweets"},
+		text_location : {label : "location"},
+		text_coordinates : {label : "coordinates"},
+		text_replytouser : {label : "replytouser"},
+		user_location : {label : "user_location"},
+		user_followers : {label : "followers"},
+		user_following : {label : "following"},
+		user_tweets : {label : "tweets"},
+		user_description : {
+					label : "description",
+					pattern : '<p style="margin-left: 5px;"><%=val%></p>'
+			},
+		user_url : {label : "website", type : "url"},
+		user_created : {label : "user_since"}
+	}
+};
+
 settings.corpora.gdc = {
 	id : "gdc",
 	title : "Gothenburg Dialogue Corpus (GDC)",
