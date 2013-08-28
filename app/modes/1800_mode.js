c2c55d992d55f184369195f1b1254b907db0e354
malin <malin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2013-08-28 11:57:00 +0000

    1917 bibel, new version
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@77431 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/1800_mode.js
+++ app/modes/1800_mode.js
@@ -15,84 +15,69 @@ settings.corporafolders = {};
 
 
 settings.corpora.bibel1917 = {
-	id : "bibel1917",
-	title : "Bibeln 1917",
-	description : "",
-	within : settings.defaultWithin,
-	context : settings.defaultContext,
-	attributes : {
-		pos : attrs.pos,
-		msd : attrs.msd,
-		lemma : attrs.baseform,
-		lex : attrs.lemgram,
-		saldo : attrs.saldo,
-		prefix : attrs.prefix,
-		suffix : attrs.suffix,
-		dephead : attrs.dephead,
-		deprel : attrs.deprel,
-		ref : attrs.ref,
-	},
-	struct_attributes : {
-		"text_title" : {label : "title"},
-		"chapter_name" : {label : "chapter"},
-		"verse_name" : {label : "verse"},
-		"text_date" : {label : "year"}
-	}
+    id : "bibel1917",
+    title : "Bibeln 1917",
+    description : "",
+    within : settings.defaultWithin,
+    context : settings.spContext,
+    attributes : {
+        pos : attrs.pos,
+        msd : attrs.msd,
+        lemma : attrs.baseform,
+        lex : attrs.lemgram,
+        saldo : attrs.saldo,
+        prefix : attrs.prefix,
+        suffix : attrs.suffix,
+        dephead : attrs.dephead,
+        deprel : attrs.deprel,
+        ref : attrs.ref,
+        texttype : {
+             label : "fab_texttype",
+             type : "set",
+             displayType : "select",
+             translationKey : "fab_",
+             dataset : [
+                 "info",
+                 "footnote",
+             ],
+             opts : settings.liteOptions
+        
+       },
+
+    },
+    struct_attributes : {
+        "text_title" : {label : "chapter"},
+        "text_subtitle" : {label : "title"},
+        "verse_name" : {label : "verse"},
+        "text_date" : {label : "year"}
+    }
 };
 
 settings.corpora.bibel1873dalin = {
-	morf : 'saldom|dalinm|swedbergm',
-	id : "bibel1873dalin",
-	title : "Bibeln 1873",
-	within : settings.defaultWithin,
-	context : settings.defaultContext,
-	attributes : {
-		pos : attrs.pos,
-		msd : attrs.msd,
-		lemma    : attrs.baseform,
-		lex : attrs.lemgram,
-		prefix : attrs.prefix,
-		suffix : attrs.suffix,
-		dephead : attrs.dephead,
-		deprel : attrs.deprel,
-		ref : attrs.ref
-		
-	},
-	struct_attributes : {
-		"text_title" : {label : "title"},
-		"chapter_name" : {label : "chapter"},
-		"verse_name" : {label : "verse"},
-		"text_date" : {label : "year"}
-	}
-};
-
-/*
-settings.corpora.bibel1873 = {
-	id : "bibel1873",
-	title : "Bibeln 1873",
-	description : "",
+    morf : 'saldom|dalinm|swedbergm',
+    id : "bibel1873dalin",
+    title : "Bibeln 1873",
     within : settings.defaultWithin,
-    context : settings.defaultContext,
-	attributes : {
-		pos : attrs.pos,
-		msd : attrs.msd,
-		lemma : attrs.baseform,
-		lex : attrs.lemgram,
-		saldo : attrs.saldo,
-		prefix : attrs.prefix,
-		suffix : attrs.suffix,
-		dephead : attrs.dephead,
-		deprel : attrs.deprel,
-		ref : attrs.ref
-	},
-	struct_attributes : {
-		"text_title" : {label : "title"},
+    context : settings.spContext,
+    attributes : {
+        pos : attrs.pos,
+        msd : attrs.msd,
+        lemma : attrs.baseform,
+        lex : attrs.lemgram,
+        prefix : attrs.prefix,
+        suffix : attrs.suffix,
+        dephead : attrs.dephead,
+        deprel : attrs.deprel,
+        ref : attrs.ref
+        
+    },
+    struct_attributes : {
+        "text_title" : {label : "title"},
         "chapter_name" : {label : "chapter"},
         "verse_name" : {label : "verse"},
-		"text_date" : {label : "year"}
-	}
+        "text_date" : {label : "year"}
+    }
 };
-*/
 
 
 settings.corpusListing = new CorpusListing(settings.corpora);
