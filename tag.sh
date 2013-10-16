6cd51047ad35371a4231e5c8e9c8bb9e5d761592
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2013-10-16 15:35:00 +0000

    changing tag location
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@80770 ef392f78-ee14-0410-84e5-9db46baae5bb

--- tag.sh
+++ tag.sh
@@ -6,4 +6,4 @@ if [ $# -ne 1 ]; then
 fi
 DATE=$(date +"%Y-%m-%d %H:%M")
 svn copy https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend \
-      https://svn.spraakdata.gu.se/sb-arkiv/pub/ri/korp_releases/"korp $1 $DATE" -m "Release $1"
+      https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/pub/releases/korp\-frontend/"korp $1 $DATE" -m "Release $1"
