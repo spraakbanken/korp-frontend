92bb5da946343d4e0a708d23fbd07bc9ca208e46
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-01-28 11:02:39 +0000

    Nya korpusar
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@88133 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/fisk1800_mode.js
+++ app/modes/fisk1800_mode.js
@@ -25,6 +25,18 @@ settings.corporafolders.bd1800 = {
 	description : "Privatkorrespondens, dagböcker, resejournaler och andra icke skönlitterära texter såsom meddelanden och uppsatser."
 };
 
+settings.corporafolders.sakprosa = {
+	title : "Sakprosa",
+	contents : ["sakprosa1750-1799", "sakprosa1800-1849", "sakprosa1850-1899", "sakprosa1900-1959"],
+	description : ""
+};
+
+settings.corporafolders.tidskrifter = {
+	title : "Tidskrifter",
+	contents : ["filosofia1850-1899", "filosofia1900-1959"],
+	description : ""
+};
+
 settings.corpora.fsbbrev1700tal = {
 	id : "fsbbrev1700tal",
 	title : "Brev 1700-tal",
@@ -135,5 +147,178 @@ settings.corpora["fsbskonlit1900-1959"] = {
 		"text_publisher" : {label : "publisher"}
 	}
 };
+
+settings.corpora["filosofia1850-1899"] = {
+	id : "filosofia1850-1899",
+	title : "Filosofia.fi 1850–1899",
+	description : "Tidskriftstexter från webbsidan filosofia.fi",
+	morf : 'saldom|dalinm|swedbergm',
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+	},
+	struct_attributes : {
+		"text_author" : {label : "author"},
+		"text_title" : {label : "title"},
+		"text_date" : {label : "year"},
+		"text_source" : {label : "source"},
+		"text_url" : {label : "url", type : "url"}
+	}
+};
+
+settings.corpora["filosofia1900-1959"] = {
+	id : "filosofia1900-1959",
+	title : "Filosofia.fi 1900–1959",
+	description : "Tidskriftstexter från webbsidan filosofia.fi",
+	morf : 'saldom|dalinm|swedbergm',
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+	},
+	struct_attributes : {
+		"text_author" : {label : "author"},
+		"text_title" : {label : "title"},
+		"text_date" : {label : "year"},
+		"text_source" : {label : "source"},
+		"text_url" : {label : "url", type : "url"}
+	}
+};
+
+settings.corpora["sakprosa1750-1799"] = {
+	id : "sakprosa1750-1799",
+	title : "Sakprosa 1750–1799",
+	description : "",
+	morf : 'saldom|dalinm|swedbergm',
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+	},
+	struct_attributes : {
+		"text_author" : {label : "author"},
+		"text_title" : {label : "title"},
+		"text_date" : {label : "year"},
+		"text_publisher" : {label : "publisher"},
+		"text_url" : {label : "url", type : "url"},
+		"text_pages" : {label : "page"}
+	}
+};
+
+settings.corpora["sakprosa1800-1849"] = {
+	id : "sakprosa1800-1849",
+	title : "Sakprosa 1800–1849",
+	description : "",
+	morf : 'saldom|dalinm|swedbergm',
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+	},
+	struct_attributes : {
+		"text_author" : {label : "author"},
+		"text_title" : {label : "title"},
+		"text_date" : {label : "year"},
+		"text_publisher" : {label : "publisher"},
+		"text_url" : {label : "url", type : "url"},
+		"text_pages" : {label : "page"}
+	}
+};
+
+settings.corpora["sakprosa1850-1899"] = {
+	id : "sakprosa1850-1899",
+	title : "Sakprosa 1850–1899",
+	description : "",
+	morf : 'saldom|dalinm|swedbergm',
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+	},
+	struct_attributes : {
+		"text_author" : {label : "author"},
+		"text_title" : {label : "title"},
+		"text_date" : {label : "year"},
+		"text_publisher" : {label : "publisher"},
+		"text_url" : {label : "url", type : "url"},
+		"text_pages" : {label : "page"}
+	}
+};
+
+settings.corpora["sakprosa1900-1959"] = {
+	id : "sakprosa1900-1959",
+	title : "Sakprosa 1900–1959",
+	description : "",
+	morf : 'saldom|dalinm|swedbergm',
+	within : settings.defaultWithin,
+	context : settings.defaultContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+	},
+	struct_attributes : {
+		"text_author" : {label : "author"},
+		"text_title" : {label : "title"},
+		"text_date" : {label : "year"},
+		"text_publisher" : {label : "publisher"},
+		"text_url" : {label : "url", type : "url"},
+		"text_pages" : {label : "page"}
+	}
+};
+
 
 settings.corpusListing = new CorpusListing(settings.corpora);
