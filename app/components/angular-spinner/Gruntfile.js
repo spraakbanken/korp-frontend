4c86e90973a4186f147449ea89d25bbf333b2222
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-02-18 13:50:29 +0000

    added missing components
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@89835 ef392f78-ee14-0410-84e5-9db46baae5bb

---
+++ app/components/angular-spinner/Gruntfile.js
@@ -0,0 +1,48 @@
+/* License: MIT.
+ * Copyright (C) 2013, Uri Shaked.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+	// load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		karma: {
+			unit: {
+				configFile: 'karma.conf.js',
+				singleRun: true
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'angular-spinner.js',
+				'tests.js',
+				'karma.conf.js'
+			]
+		},
+		uglify: {
+			dist: {
+				files: {
+					'angular-spinner.min.js': 'angular-spinner.js'
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('test', [
+		'jshint',
+		'karma'
+	]);
+
+	grunt.registerTask('build', [
+		'uglify'
+	]);
+
+	grunt.registerTask('default', ['build']);
+};
