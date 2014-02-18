d64a58ce9985939042b6e012dec8820d6ca8da87
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-02-18 13:51:13 +0000

    working on build step
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@89871 ef392f78-ee14-0410-84e5-9db46baae5bb

--- Gruntfile.js
+++ Gruntfile.js
@@ -194,7 +194,8 @@ module.exports = function (grunt) {
       }
     },
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      // html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         dirs: ['<%= yeoman.dist %>']
