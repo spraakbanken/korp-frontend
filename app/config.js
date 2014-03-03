0472e76a63aaa75cf2cdbeb4bcdff7336fe7949a
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-03-03 13:35:54 +0000

    Ny korpus
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@90971 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/config.js
+++ app/config.js
@@ -681,7 +681,7 @@ settings.corporafolders.socialmedia.forum = {
 
 settings.corporafolders.socialmedia.forum.flashback = {
 	title : "Flashback",
-	contents : ["flashback-resor"],
+	contents : ["flashback-dator", "flashback-resor"],
 	description : "Material från diskussionsforumet <a target=\"_blank\" href=\"https://www.flashback.org/\">Flashback</a>. Materialet är under uppbyggnad."
 };
 
@@ -1758,6 +1758,38 @@ settings.corpora.strindbergbrev = {
         text_day : {label : "day"},
         text_volume : {label : "text_volume"}
     }
+};
+
+settings.corpora["flashback-dator"] = {
+	id : "flashback-dator",
+	title : "Flashback: Dator & IT",
+	description : "",
+	within : settings.spWithin,
+	context : settings.spContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix
+	},
+	struct_attributes : {
+        text_username : {label : "username2"},
+        text_date : {label : "date"},
+        text_links : {label : "postlinks", type : "set"},
+        text_url : {label : "posturl", type : "url"},
+        thread_title : {label : "thread"},
+        thread_postcount : {label : "threadpostcount"},
+        thread_lastpost : {label : "threadlastpost"},
+        thread_url : {label : "thread", type : "url"},
+        forum_title : {label : "forum"},
+        forum_url : {label : "forum", type : "url"}
+	}
 };
 
 settings.corpora["flashback-resor"] = {
