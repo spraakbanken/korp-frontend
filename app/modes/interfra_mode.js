5fac55b233f83f992fa2605e5e9b66aee42aec94
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-03-07 14:10:53 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@91373 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/interfra_mode.js
+++ app/modes/interfra_mode.js
@@ -21,8 +21,34 @@ settings.corpora.interfra = {
 	},
 	struct_attributes : {
 		"u_who" : {label : "speaker"},
-		"text_part" : {label : "part"},
-		"text_group" : {label : "group"},
+		"text_part" : {
+		    label : "part",
+		    displayType : "select",
+		    dataset : {"1A" : "1A", "1B" : "1B", "2" : "2"},
+		    controller : selectType.controller,
+		    extended_template : selectType.extended_template
+		},
+		"text_group" : {
+		    label : "group",
+		    displayType : "select",
+		    dataset : {
+                "G" : "Secondary school students (G)",
+                "N" : "Beginners (N)",
+                "L" : "1st and 2nd years’ university students (L)",
+                "T" : "1st and 2nd years’ university students (T)",
+                "R" : "Future teachers (R)",
+                "D" : "PhD students (D)",
+                "F" : "FSL juniors (F)",
+                "Q" : "FSL seniors (Q)",
+                "M" : "Multi-task group NNS (M)",
+                "C" : "Erasmus exchange control group (C)",
+                "J" : "Control group of junior NS (J)",
+		        "S" : "Control group of senior NS (S)",
+                "K" : "Multi-task control group (K)"
+                },
+		    controller : selectType.controller,
+		    extended_template : selectType.extended_template
+		},
 		"text_activity" : {label : "activity"},
 		"text_interviewee" : {label : "interviewee"},
 		"text_activity_date" : {label : "date"}/*,
