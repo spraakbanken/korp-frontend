ef4f8e3a38f29dc892cca394e5df4f8fc9760118
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-05-07 15:48:03 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@95330 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/interfra_mode.js
+++ app/modes/interfra_mode.js
@@ -59,4 +59,62 @@ settings.corpora.interfra = {
 	},
 };
 
+settings.corpora["interfra-sv"] = {
+	id : "interfra-sv",
+	title : "InterFra svenska",
+	description : "",
+	within : settings.spWithin,
+	context : settings.spContext,
+	attributes : {
+		pos : attrs.pos,
+		msd : attrs.msd,
+		lemma : attrs.baseform,
+		lex : attrs.lemgram,
+		saldo : attrs.saldo,
+		dephead : attrs.dephead,
+		deprel : attrs.deprel,
+		ref : attrs.ref,
+		prefix : attrs.prefix,
+		suffix : attrs.suffix
+	},
+	struct_attributes : {
+		"u_who" : {label : "speaker"},
+		"text_part" : {
+		    label : "part",
+		    displayType : "select",
+		    dataset : {"1A" : "1A", "1B" : "1B", "2" : "2"},
+		    controller : selectType.controller,
+		    extended_template : selectType.extended_template
+		},
+		"text_group" : {
+		    label : "group",
+		    displayType : "select",
+		    dataset : {
+                "G" : "Secondary school students (G)",
+                "N" : "Beginners (N)",
+                "L" : "1st and 2nd years’ university students (L)",
+                "T" : "1st and 2nd years’ university students (T)",
+                "R" : "Future teachers (R)",
+                "D" : "PhD students (D)",
+                "F" : "FSL juniors (F)",
+                "Q" : "FSL seniors (Q)",
+                "M" : "Multi-task group NNS (M)",
+                "C" : "Erasmus exchange control group (C)",
+                "J" : "Control group of junior NS (J)",
+		        "S" : "Control group of senior NS (S)",
+                "K" : "Multi-task control group (K)"
+                },
+		    controller : selectType.controller,
+		    extended_template : selectType.extended_template
+		},
+		"text_activity" : {label : "activity"},
+		"text_interviewee" : {label : "interviewee"},
+		"text_activity_date" : {label : "date"}/*,
+		"text_transcriber" : {label : ""},
+		"text_transcription_date" : {label : ""},
+		"text_transcription_checker" : {label : ""},
+		"text_check_date" : {label : ""}*/
+	},
+};
+
 settings.corpusListing = new CorpusListing(settings.corpora);
