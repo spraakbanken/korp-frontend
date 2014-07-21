55460684166be679b1f027f465e095f6e05aaa72
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-07-21 10:07:52 +0000

    +Familjeliv: Adoption
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@99526 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/config.js
+++ app/config.js
@@ -656,7 +656,7 @@ settings.corporafolders.socialmedia.forum = {
 
 settings.corporafolders.socialmedia.forum.familjeliv = {
 	title : "Familjeliv",
-	contents : ["familjeliv-pappagrupp"],
+	contents : ["familjeliv-adoption", "familjeliv-pappagrupp"],
 	description : "Material från diskussionsforumet <a target=\"_blank\" href=\"https://www.familjeliv.se/\">Familjeliv</a>. Materialet är under uppbyggnad."
 };
 
@@ -1586,6 +1586,27 @@ settings.corpora.strindbergbrev = {
         text_day : {label : "day"},
         text_volume : {label : "text_volume"}
     }
+};
+
+settings.corpora["familjeliv-adoption"] = {
+	id : "familjeliv-adoption",
+	title : "Familjeliv: Adoption",
+	description : "",
+	within : settings.spWithin,
+	context : settings.spContext,
+	attributes : modernAttrs,
+	struct_attributes : {
+        text_username : {label : "username2"},
+        text_date : {label : "date"},
+        text_links : {label : "postlinks", type : "set"},
+        text_url : {label : "posturl", type : "url"},
+        thread_title : {label : "thread"},
+        thread_postcount : {label : "threadpostcount"},
+        thread_lastpost : {label : "threadlastpost"},
+        thread_url : {label : "thread", type : "url"},
+        forum_title : {label : "forum"},
+        forum_url : {label : "forum", type : "url"}
+	}
 };
 
 settings.corpora["familjeliv-pappagrupp"] = {
