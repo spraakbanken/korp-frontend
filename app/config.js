c7fda742babe08c6c9e19656dca92f6474413d38
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-10-01 09:34:59 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@103517 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/config.js
+++ app/config.js
@@ -661,7 +661,7 @@ settings.corporafolders.socialmedia.forum = {
 
 settings.corporafolders.socialmedia.forum.familjeliv = {
 	title : "Familjeliv",
-	contents : ["familjeliv-adoption", "familjeliv-allmanna-ekonomi", "familjeliv-allmanna-familjeliv", "familjeliv-allmanna-fritid", "familjeliv-expert", "familjeliv-foralder", "familjeliv-gravid", "familjeliv-kansliga", "familjeliv-medlem-allmanna", "familjeliv-medlem-foraldrar", "familjeliv-medlem-planerarbarn", "familjeliv-medlem-vantarbarn", "familjeliv-pappagrupp", "familjeliv-planerarbarn", "familjeliv-sexsamlevnad", "familjeliv-svartattfabarn", "familjeliv-anglarum"],
+	contents : ["familjeliv-adoption", "familjeliv-allmanna-ekonomi", "familjeliv-allmanna-familjeliv", "familjeliv-allmanna-fritid", "familjeliv-allmanna-hushem", "familjeliv-expert", "familjeliv-foralder", "familjeliv-gravid", "familjeliv-kansliga", "familjeliv-medlem-allmanna", "familjeliv-medlem-foraldrar", "familjeliv-medlem-planerarbarn", "familjeliv-medlem-vantarbarn", "familjeliv-pappagrupp", "familjeliv-planerarbarn", "familjeliv-sexsamlevnad", "familjeliv-svartattfabarn", "familjeliv-anglarum"],
 	description : "Material från diskussionsforumet <a target=\"_blank\" href=\"https://www.familjeliv.se/\">Familjeliv</a>. Materialet är under uppbyggnad."
 };
 
@@ -1634,6 +1634,28 @@ settings.corpora["familjeliv-allmanna-familjeliv"] = {
         forum_url : {label : "forum", type : "url"}
 	}
 };
+
+settings.corpora["familjeliv-allmanna-hushem"] = {
+	id : "familjeliv-allmanna-hushem",
+	title : "Familjeliv: Allmänna rubriker – Hus & hem",
+	description : "",
+	within : settings.spWithin,
+	context : settings.spContext,
+	attributes : modernAttrs,
+	struct_attributes : {
+        text_username : {label : "username2"},
+        text_date : {label : "date"},
+        text_links : {label : "postlinks", type : "set"},
+        text_url : {label : "posturl", type : "url"},
+        thread_title : {label : "thread"},
+        thread_postcount : {label : "threadpostcount"},
+        thread_lastpost : {label : "threadlastpost"},
+        thread_url : {label : "thread", type : "url"},
+        forum_title : {label : "forum"},
+        forum_url : {label : "forum", type : "url"}
+	}
+};
+
 
 settings.corpora["familjeliv-allmanna-fritid"] = {
 	id : "familjeliv-allmanna-fritid",
