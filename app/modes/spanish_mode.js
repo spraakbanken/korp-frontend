505441a19b0ed740323d9090703266592df994f7
anne <anne@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-10-09 07:35:13 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@103949 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/spanish_mode.js
+++ app/modes/spanish_mode.js
@@ -12,7 +12,7 @@ settings.corporafolders = {};
 settings.corpora["pe77"] = {
 	id : "pe77",
 	lang : "spa",
-	title: "Spanska Presstexter",
+	title: "Spanska presstexter",
 	context: settings.defaultContext,
 	within : settings.defaultWithin,
 	attributes: {
@@ -32,5 +32,30 @@ settings.corpora["pe77"] = {
 		}
 	}
 };
+
+settings.corpora["one71"] = {
+	id : "one71",
+	lang : "spa",
+	title: "Spanska noveller",
+	context: settings.defaultContext,
+	within : settings.defaultWithin,
+	attributes: {
+		pos: {label : "pos"},
+		lemma: {label : "baseform"}
+	},
+	struct_attributes : {
+		text_title : {label : "title"},
+		text_lang : {
+			label : "lang",
+			displayType : "select",
+			extended_template : selectType.extended_template,
+			controller : selectType.controller,
+			dataset: {
+				"spa" : "spanish"
+			}
+		}
+	}
+};
+
 
 settings.corpusListing = new CorpusListing(settings.corpora);
