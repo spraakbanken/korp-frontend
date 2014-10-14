d87fa1e54813671495f1aa251095f20e86753aa6
anne <anne@ef392f78-ee14-0410-84e5-9db46baae5bb>
2014-10-14 10:16:14 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@104228 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/spanish_mode.js
+++ app/modes/spanish_mode.js
@@ -12,7 +12,8 @@ settings.corporafolders = {};
 settings.corpora["pe77"] = {
 	id : "pe77",
 	lang : "spa",
-	title: "Spanska presstexter",
+	title: "Banco de Datos de Prensa Española 1977 (SOL)",
+	description : "spanska presstexter från tidskrifterna 'El País' och 'Triunfo'",
 	context: settings.defaultContext,
 	within : settings.defaultWithin,
 	attributes: {
@@ -36,7 +37,8 @@ settings.corpora["pe77"] = {
 settings.corpora["one71"] = {
 	id : "one71",
 	lang : "spa",
-	title: "Spanska noveller",
+	title: "Banco de Datos de Once Novelas Españolas 1951—1971 (SOL)",
+	description : "11 spanska noveller",
 	context: settings.defaultContext,
 	within : settings.defaultWithin,
 	attributes: {
