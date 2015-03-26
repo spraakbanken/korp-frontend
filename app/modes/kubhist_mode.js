8f0079d94fb022a5380895047dfea51bedd65019
anne <anne@ef392f78-ee14-0410-84e5-9db46baae5bb>
2015-03-26 11:09:24 +0000

    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@114047 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/kubhist_mode.js
+++ app/modes/kubhist_mode.js
@@ -25,6 +25,64 @@ settings.digidailystruct_attributes = {
 	text_date : {label : "date"}
 };
 
+settings.kubhistattributes = {
+	lemma : attrs.baseform,
+	pos : attrs.pos,
+	lex : attrs.lemgram,
+	dalinlex : {
+	    label : "dalin-lemgram",
+	    type : "set",
+	    displayType : "autocomplete",
+	    opts : settings.setOptions,
+	    stringify : function(lemgram) {
+	        // if(_.contains(lemgram, " "))
+	        // TODO: what if we're getting more than one consequtive lemgram back?
+	        return util.lemgramToString(_.str.trim(lemgram), true);
+	    },
+	    externalSearch : karpLemgramLink,
+	    internalSearch : true,
+	    extended_template : "<input korp-autocomplete model='model' stringify='stringify' sorter='sorter' type='lem' >",
+	    controller : function($scope) {
+	        $scope.stringify = util.lemgramToString;
+	        $scope.sorter = view.lemgramSort;
+	    }
+	},
+	dephead : attrs.dephead,
+	deprel : attrs.deprel,
+	ref : attrs.ref,
+	saldo : attrs.saldo,
+	prefix : attrs.prefix,
+	suffix : attrs.suffix
+};
+settings.kubhiststruct_attributes = {
+	text_title : {
+		label : "title",
+		displayType : "select",
+		localize : false,
+ 		opts : settings.liteOptions
+	},
+	text_date : {label : "date"},
+	text_edition : {label : "edition"},
+	text_periodofpublication : {label : "periodofpublication"},
+	text_holderofpublicationlicense : {label : "holderofpublicationlicense"},
+	text_publishingfrequency : {label : "publishingfrequency"},
+	text_publishingdays : {label : "publishingdays"},
+	text_completetitle : {label : "completetitle"},
+	text_publisher : {label : "publisher"},
+	text_issn : {label : "issn"},
+	text_politicaltendency : {label : "politicaltendency"},
+	text_annualprice : {label : "annualprice"},
+	text_editorialplace : {label : "editorialplace"},
+	text_typearea : {label : "typearea"},
+	text_numberofpages : {label : "numberofpages"},
+	text_publicationtype : {label : "publicationtype"},
+	text_editor : {label : "editor"},
+	text_printedin : {label : "printedin"},
+	text_printedby : {label : "printedby"},
+	text_commentaries : {label : "commentaries"}
+};
+
+
 digidailydescription = '<a href="http://digidaily.kb.se/">Digidaily</a> är ett utvecklingsprojekt där Riksarkivet, Kungliga biblioteket och Mittuniversitetet tillsammans ska utveckla rationella metoder och processer för digitalisering av dagstidningar.'
 	
 $("#lemgram_list_item").remove();
@@ -34,17 +92,70 @@ $("#showLineDiagram").remove();
 settings.corpora = {};
 settings.corporafolders = {};
 
-settings.corpora["digidaily-bollnastidning"] = {
-    morf : 'saldom|dalinm|swedbergm',
-	id : "digidaily-bollnastidning",
+settings.corporafolders.kubhistbollnastidning = {
 	title : "Bollnäs tidning",
+	contents : ["kubhist-bollnastidning-1870", "kubhist-bollnastidning-1880"]
+};
+
+settings.bollnastidning_struct_attributes = {
+	text_title : {
+		label : "title",
+		displayType : "select",
+		localize : false,
+ 		opts : settings.liteOptions
+	},
+	text_date : {label : "date"},
+	text_periodofpublication : {label : "periodofpublication"},
+	text_holderofpublicationlicense : {label : "holderofpublicationlicense"},
+	text_publishingfrequency : {label : "publishingfrequency"},
+	text_publishingdays : {label : "publishingdays"},
+	text_completetitle : {label : "completetitle"},
+	text_issn : {label : "issn"},
+	text_annualprice : {label : "annualprice"},
+	text_typearea : {label : "typearea"},
+	text_numberofpages : {label : "numberofpages"},
+	text_publicationtype : {label : "publicationtype"},
+	text_editor : {label : "editor"},
+	text_printedin : {label : "printedin"},
+	text_printedby : {label : "printedby"},
+	text_commentaries : {label : "commentaries"}
+};
+
+settings.corpora["kubhist-bollnastidning-1870"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "kubhist-bollnastidning-1870",
+	title : "Bollnäs tidning 1870-talet",
 	description : digidailydescription,
 	within : settings.defaultWithin,
 	context : settings.spContext,
-	attributes : settings.digidailyattributes,
-       
-	struct_attributes : settings.digidailystruct_attributes
+	attributes : settings.kubhistattributes,
+	struct_attributes : settings.bollnastidning_struct_attributes
 };
+
+settings.corpora["kubhist-bollnastidning-1880"] = {
+    morf : 'saldom|dalinm|swedbergm',
+	id : "kubhist-bollnastidning-1880",
+	title : "Bollnäs tidning 1880-talet",
+	description : digidailydescription,
+	within : settings.defaultWithin,
+	context : settings.spContext,
+	attributes : settings.kubhistattributes,
+	struct_attributes : settings.bollnastidning_struct_attributes
+};
+
+
+
+// settings.corpora["digidaily-bollnastidning"] = {
+//     morf : 'saldom|dalinm|swedbergm',
+// 	id : "digidaily-bollnastidning",
+// 	title : "Bollnäs tidning",
+// 	description : digidailydescription,
+// 	within : settings.defaultWithin,
+// 	context : settings.spContext,
+// 	attributes : settings.digidailyattributes,
+       
+// 	struct_attributes : settings.digidailystruct_attributes
+// };
 settings.corpora["digidaily-dalpilen"] = {
     morf : 'saldom|dalinm|swedbergm',
 	id : "digidaily-dalpilen",
