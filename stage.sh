3f8b79d83d4b27e3621dc430de3b50c88d9df7a8
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2015-09-17 13:31:57 +0000

    changed to fkkorp for releasing
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@121145 ef392f78-ee14-0410-84e5-9db46baae5bb

--- stage.sh
+++ stage.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git svn rebase && rsync --delete -r dist/ johanrox@k2.spraakdata.gu.se:/var/www/html_sb/korplabb
+git svn rebase && rsync --delete -r dist/ fkkorp@k2.spraakdata.gu.se:/var/www/html_sb/korplabb
