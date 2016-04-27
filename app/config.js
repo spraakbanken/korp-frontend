ce5d7a5dd8c80976eec5f239add47ac35e1d1b5e
martin <martin@ef392f78-ee14-0410-84e5-9db46baae5bb>
2016-04-27 12:45:32 +0000

    Merged korp-frontend-current
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@238021 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/config.js
+++ app/config.js
@@ -4453,9 +4453,8 @@ settings.corpora.soexempel = {
 settings.cgi_script = "https://ws.spraakbanken.gu.se/ws/korp";
 // settings.cgi_script = "http://spraakbanken.gu.se/ws/korp";
 // settings.cgi_script = "http://demosb.spraakdata.gu.se/cgi-bin/korp/korp.cgi";
-// settings.cgi_script = "http://demosb.spraakdata.gu.se/cgi-bin/korp/korp_sme.cgi";
 
-settings.download_cgi_script = "http://demosb.spraakdata.gu.se/cgi-bin/korp/korp_download.cgi";
+settings.download_cgi_script = "https://demosb.spraakdata.gu.se/cgi-bin/korp/korp_download.cgi";
 
 
 // label values here represent translation keys.
