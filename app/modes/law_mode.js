4c30bdb1b35007e0630c7c3af4d3bee2f34390b5
anne <anne@ef392f78-ee14-0410-84e5-9db46baae5bb>
2017-01-09 14:48:34 +0000

    added corpus lawline
    git-svn-id: https://svn.spraakdata.gu.se/repos/lb/trunk/sbkhs/korp-frontend@248796 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/modes/law_mode.js
+++ app/modes/law_mode.js
@@ -7,12 +7,59 @@ $("#lemgram_list_item").remove();
 $("#results-lemgram").remove();
 $("#showLineDiagram").remove();
 
+var modernAttrs2 = _.extend({}, modernAttrs, {
+    ne_ex: attrs.ne_ex,
+    ne_type: attrs.ne_type,
+    ne_subtype: attrs.ne_subtype,
+    ne_name: attrs.ne_name,
+    complemgram: {
+        label: "complemgram",
+        internalSearch: true,
+        ranked: true,
+        display: {
+            expandList: {
+                splitValue: function(value) { return value.split("+"); },
+                searchKey: "lex",
+                joinValues: " + ",
+                stringify: function(lemgram) { return util.lemgramToString(lemgram, true); },
+                linkAllValues: true
+            }
+        },
+        type: "set",
+        hideStatistics: true,
+        hideExtended: true,
+        hideCompare: true
+    },
+    compwf: {
+        label: "compwf",
+        display: {
+            "expandList": {}
+        },
+        type: "set",
+        hideStatistics: true,
+        hideExtended: true,
+        hideCompare: true
+    },
+    sense: {
+        label: "sense",
+        type: "set",
+        ranked: true,
+        display: {
+            expandList: {
+                internalSearch: function(key, value) { return "[" + key + " = '\\|" + regescape(value) + ":.*']"},
+            }
+        },
+        stringify: function(sense) { return util.saldoToString(sense, true); },
+        opts: settings.probabilitySetOptions,
+        externalSearch: "https://spraakbanken.gu.se/karp/#?search=extended||and|sense|equals|<%= val %>",
+        internalSearch: true,
+        extended_template: settings.senseAutoComplete
+    }
+});
+delete modernAttrs2.saldo;
+
 settings.corpora = {};
 settings.corporafolders = {};
-settings.corporafolders.modern = {
-    title: "Moderna lagar och rättsfall",
-    contents: ["sfs", "moderntdv"]
-};
 
 settings.corporafolders.lag1700 = {
     title: "1734 års lag och förarbeten",
@@ -24,41 +71,14 @@ settings.corporafolders.fsvlagar = {
     contents: ["fsv-aldrelagar", "fsv-yngrelagar"]
 };
 
+settings.corporafolders.modern = {
+    title: "Moderna lagar och rättsfall",
+    contents: ["sfs", "moderntdv"]
+};
 
 settings.corpora["fsv-yngrelagar"] = fsv_yngrelagar;
 
 settings.corpora["fsv-aldrelagar"] = fsv_aldrelagar;
-
-
-settings.corpora["tankebok"] = {
-    morf: 'swedbergm|dalinm',
-    id: "tankebok",
-    title: "Stockholms stads tänkeböcker",
-    description: "Stockholms stads tänkeböcker från 1626",
-    within: settings.defaultWithin,
-    context: settings.spContext,
-    attributes: {
-        posset:  settings.posset,
-        lemma: attrs.baseform,
-        lex: attrs.lemgram
-    },
-    struct_attributes: {
-        text_date: {label: "date"},
-        text_title: {
-            label: "title",
-            displayType: "select",
-            localize: false,
-            dataset: [
-                "Stockholms stads tänkebok - Koncept ",
-                "Stockholms stads tänkebok - Notariat",
-                "Stockholms stads tänkebok - Renskr "
-            ],
-            opts: settings.liteOptions
-
-        },
-        paragraph_marginal: {label: "paragraph_marginal"}
-    }
-};
 
 
 settings.corpora["lag1734"] = {
@@ -154,6 +174,54 @@ settings.corpora["forarbeten1734"] = {
     }
 };
 
+settings.corpora["sfs"] = {
+    id: "sfs",
+    title: "Svensk författningssamling",
+    description: "",
+    within: settings.defaultWithin,
+    context: settings.spContext,
+    attributes: {
+        posset: settings.posset,
+        msd: attrs.msd,
+        lemma: attrs.baseform,
+        lex: attrs.lemgram,
+        saldo: attrs.saldo,
+        prefix: attrs.prefix,
+        suffix: attrs.suffix,
+        dephead: attrs.dephead,
+        deprel: attrs.deprel,
+        ref: attrs.ref
+    },
+    struct_attributes: {
+        text_date: {label: "date"},
+        text_title: {label: "title"}
+    }
+};
+
+settings.corpora["moderntdv"] = {
+    id: "moderntdv",
+    title: "Domar",
+    description: "",
+    within: settings.defaultWithin,
+    context: settings.spContext,
+    attributes: {
+        posset: settings.posset,
+        msd: attrs.msd,
+        lemma: attrs.baseform,
+        lex: attrs.lemgram,
+        saldo: attrs.saldo,
+        prefix: attrs.prefix,
+        suffix: attrs.suffix,
+        dephead: attrs.dephead,
+        deprel: attrs.deprel,
+        ref: attrs.ref
+    },
+    struct_attributes: {
+        text_date: {label: "date"},
+        text_title: {label: "title"}
+    }
+};
+
 settings.corpora["lag1800"] = {
     morf: 'saldom|dalinm',
     id: "lag1800",
@@ -185,53 +253,67 @@ settings.corpora["lag1800"] = {
     }
 };
 
-settings.corpora["sfs"] = {
-    id: "sfs",
-    title: "Svensk författningssamling",
-    description: "",
+settings.corpora["lawline"] = {
+    id: "lawline",
+    title: "Lawline",
+    description: "Frågor och svar kring juridisk rådgivning från lawline.",
     within: settings.defaultWithin,
     context: settings.spContext,
-    attributes: {
-        posset: settings.posset,
-        msd: attrs.msd,
-        lemma: attrs.baseform,
-        lex: attrs.lemgram,
-        saldo: attrs.saldo,
-        prefix: attrs.prefix,
-        suffix: attrs.suffix,
-        dephead: attrs.dephead,
-        deprel: attrs.deprel,
-        ref: attrs.ref
-    },
+    attributes: modernAttrs2,
     struct_attributes: {
-        text_date: {label: "date"},
-        text_title: {label: "title"}
-    }
-};
-settings.corpora["moderntdv"] = {
-    id: "moderntdv",
-    title: "Domar",
-    description: "",
-    within: settings.defaultWithin,
-    context: settings.spContext,
-    attributes: {
-        posset: settings.posset,
-        msd: attrs.msd,
-        lemma: attrs.baseform,
-        lex: attrs.lemgram,
-        saldo: attrs.saldo,
-        prefix: attrs.prefix,
-        suffix: attrs.suffix,
-        dephead: attrs.dephead,
-        deprel: attrs.deprel,
-        ref: attrs.ref
-    },
-    struct_attributes: {
-        text_date: {label: "date"},
-        text_title: {label: "title"}
+        "text_category": {label: "category", order: 10},
+        "text_topic": {label: "topic", order: 9},
+        "text_datum": {label: "date", order: 8},
+        "textpart_type": {label: "type",
+                          order: 7,
+                          displayType: "select",
+                          extended_template: selectType.extended_template,
+                          controller: selectType.controller,
+                          dataset: {
+                            "answer": "answer",
+                            "question": "question"
+                            }
+                        },
+        "text_url": {label: "url",
+                     order: 6,
+                     pattern: "<a href='<%= struct_attrs.text_url %>' target='_blank'><%= struct_attrs.text_url %></a>"
+                 },
+        "text_category_link": {label: "category_url",
+                               order: 5,
+                               pattern: "<a href='<%= struct_attrs.text_category_link %>' target='_blank'><%= struct_attrs.text_category_link %></a>"
+                          }
     }
 };
 
+settings.corpora["tankebok"] = {
+    morf: 'swedbergm|dalinm',
+    id: "tankebok",
+    title: "Stockholms stads tänkeböcker",
+    description: "Stockholms stads tänkeböcker från 1626",
+    within: settings.defaultWithin,
+    context: settings.spContext,
+    attributes: {
+        posset:  settings.posset,
+        lemma: attrs.baseform,
+        lex: attrs.lemgram
+    },
+    struct_attributes: {
+        text_date: {label: "date"},
+        text_title: {
+            label: "title",
+            displayType: "select",
+            localize: false,
+            dataset: [
+                "Stockholms stads tänkebok - Koncept ",
+                "Stockholms stads tänkebok - Notariat",
+                "Stockholms stads tänkebok - Renskr "
+            ],
+            opts: settings.liteOptions
+
+        },
+        paragraph_marginal: {label: "paragraph_marginal"}
+    }
+};
 
 
 settings.corpusListing = new CorpusListing(settings.corpora);
