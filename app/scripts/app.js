8cdc4945eca1335a93e962f65069678a1e841dbd
Johan Roxendal <johan.roxendal@gu.se>
2018-11-29 10:49:47 +0100

    finished app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,66 +1,58 @@
-/* eslint-disable
-    no-unused-vars,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS104: Avoid inline assignments
- * DS204: Change includes calls to have a more natural evaluation order
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-window.korpApp = angular.module('korpApp', [
-                                            "ui.bootstrap.typeahead",
-                                            "uib/template/typeahead/typeahead-popup.html",
-                                            "uib/template/typeahead/typeahead-match.html",
-                                            "ui.bootstrap.tooltip",
-                                            "uib/template/tooltip/tooltip-popup.html",
-                                            "uib/template/tooltip/tooltip-html-popup.html",
-                                            "ui.bootstrap.modal",
-                                            "uib/template/modal/window.html",
-                                            "ui.bootstrap.tabs",
-                                            "uib/template/tabs/tabset.html",
-                                            "uib/template/tabs/tab.html",
-                                            "ui.bootstrap.dropdown",
-                                            "ui.bootstrap.pagination",
-                                            "uib/template/pagination/pagination.html",
-                                            "ui.bootstrap.datepicker",
-                                            "uib/template/datepicker/datepicker.html",
-                                            "uib/template/datepicker/day.html",
-                                            "uib/template/datepicker/month.html",
-                                            "uib/template/datepicker/year.html",
-                                            "ui.bootstrap.timepicker",
-                                            "uib/template/timepicker/timepicker.html",
-                                            "ui.bootstrap.buttons",
-                                            "angularSpinner",
-                                            "ui.sortable",
-                                            "newsdesk",
-                                            "sbMap",
-                                            "tmh.dynamicLocale",
-                                            "angular.filter"
-                                        ])
+/** @format */
+window.korpApp = angular.module("korpApp", [
+    "ui.bootstrap.typeahead",
+    "uib/template/typeahead/typeahead-popup.html",
+    "uib/template/typeahead/typeahead-match.html",
+    "ui.bootstrap.tooltip",
+    "uib/template/tooltip/tooltip-popup.html",
+    "uib/template/tooltip/tooltip-html-popup.html",
+    "ui.bootstrap.modal",
+    "uib/template/modal/window.html",
+    "ui.bootstrap.tabs",
+    "uib/template/tabs/tabset.html",
+    "uib/template/tabs/tab.html",
+    "ui.bootstrap.dropdown",
+    "ui.bootstrap.pagination",
+    "uib/template/pagination/pagination.html",
+    "ui.bootstrap.datepicker",
+    "uib/template/datepicker/datepicker.html",
+    "uib/template/datepicker/day.html",
+    "uib/template/datepicker/month.html",
+    "uib/template/datepicker/year.html",
+    "ui.bootstrap.timepicker",
+    "uib/template/timepicker/timepicker.html",
+    "ui.bootstrap.buttons",
+    "angularSpinner",
+    "ui.sortable",
+    "newsdesk",
+    "sbMap",
+    "tmh.dynamicLocale",
+    "angular.filter"
+])
 
-korpApp.config(tmhDynamicLocaleProvider => tmhDynamicLocaleProvider.localeLocationPattern("translations/angular-locale_{{locale}}.js"))
+korpApp.config(tmhDynamicLocaleProvider =>
+    tmhDynamicLocaleProvider.localeLocationPattern("translations/angular-locale_{{locale}}.js")
+)
 
 korpApp.config($uibTooltipProvider =>
     $uibTooltipProvider.options({
-        appendToBody: true })
+        appendToBody: true
+    })
 )
 
-korpApp.config(['$locationProvider', $locationProvider => $locationProvider.hashPrefix('')
-])
+korpApp.config(["$locationProvider", $locationProvider => $locationProvider.hashPrefix("")])
 
-korpApp.config(['$compileProvider', $compileProvider => $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/)
+korpApp.config([
+    "$compileProvider",
+    $compileProvider =>
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/)
 ])
 
 korpApp.run(function($rootScope, $location, utils, searches, tmhDynamicLocale, $timeout, $q) {
-    let corpus, loginNeededFor
+    let loginNeededFor
     const s = $rootScope
     s._settings = settings
-    window.lang = (s.lang = $location.search().lang || settings.defaultLanguage)
+    window.lang = s.lang = $location.search().lang || settings.defaultLanguage
     s.word_selected = null
     s.isLab = window.isLab
 
@@ -82,12 +74,10 @@ korpApp.run(function($rootScope, $location, utils, searches, tmhDynamicLocale, $
 
     s.$watch("_loc.search()", function() {
         c.log("loc.search() change", $location.search())
-        _.defer(() => typeof window.onHashChange === 'function' ? window.onHashChange() : undefined)
+        _.defer(() => (window.onHashChange || _.noop)())
 
         return tmhDynamicLocale.set($location.search().lang || "sv")
     })
-
-
 
     $rootScope.kwicTabs = []
     $rootScope.compareTabs = []
@@ -97,11 +87,13 @@ korpApp.run(function($rootScope, $location, utils, searches, tmhDynamicLocale, $
 
     if ($location.search().corpus) {
         loginNeededFor = []
-        for (corpus of Array.from($location.search().corpus.split(","))) {
+        for (let corpus of $location.search().corpus.split(",")) {
             const corpusObj = settings.corpusListing.struct[corpus]
             if (corpusObj.limitedAccess) {
-                var needle
-                if ((_.isEmpty(authenticationProxy.loginObj)) || ((needle = corpus.toUpperCase(), !Array.from(authenticationProxy.loginObj.credentials).includes(needle)))) {
+                if (
+                    _.isEmpty(authenticationProxy.loginObj) ||
+                    !authenticationProxy.loginObj.credentials.includes(corpus.toUpperCase())
+                ) {
                     loginNeededFor.push(corpusObj)
                 }
             }
@@ -134,13 +126,15 @@ korpApp.run(function($rootScope, $location, utils, searches, tmhDynamicLocale, $
         }
     }
 
-
     s.searchDisabled = false
     s.$on("corpuschooserchange", function(event, corpora) {
         c.log("corpuschooserchange", corpora)
         settings.corpusListing.select(corpora)
         const nonprotected = _.map(settings.corpusListing.getNonProtected(), "id")
-        if (corpora.length && (_.intersection(corpora, nonprotected).length !== nonprotected.length)) {
+        if (
+            corpora.length &&
+            _.intersection(corpora, nonprotected).length !== nonprotected.length
+        ) {
             $location.search("corpus", corpora.join(","))
         } else {
             $location.search("corpus", null)
@@ -151,19 +145,25 @@ korpApp.run(function($rootScope, $location, utils, searches, tmhDynamicLocale, $
         s.searchDisabled = settings.corpusListing.selected.length === 0
     })
 
-    return searches.infoDef.then(function() {
-        ({ corpus } = $location.search())
+    searches.infoDef.then(function() {
+        let { corpus } = $location.search()
         let currentCorpora = []
         if (corpus) {
-            currentCorpora = _.flatten(_.map(corpus.split(","), val => 
-                getAllCorporaInFolders(settings.corporafolders, val)))
+            currentCorpora = _.flatten(
+                _.map(corpus.split(","), val =>
+                    getAllCorporaInFolders(settings.corporafolders, val)
+                )
+            )
         } else {
-            if (!(settings.preselectedCorpora != null ? settings.preselectedCorpora.length : undefined)) {
+            if (!(settings.preselectedCorpora && settings.preselectedCorpora.length)) {
                 currentCorpora = _.map(settings.corpusListing.corpora, "id")
             } else {
                 for (let pre_item of settings.preselectedCorpora) {
-                    pre_item = pre_item.replace(/^__/g, '')
-                    currentCorpora = [].concat(currentCorpora, getAllCorporaInFolders(settings.corporafolders, pre_item))
+                    pre_item = pre_item.replace(/^__/g, "")
+                    currentCorpora = [].concat(
+                        currentCorpora,
+                        getAllCorporaInFolders(settings.corporafolders, pre_item)
+                    )
                 }
             }
 
@@ -171,23 +171,25 @@ korpApp.run(function($rootScope, $location, utils, searches, tmhDynamicLocale, $
         }
 
         settings.corpusListing.select(currentCorpora)
-        return corpusChooserInstance.corpusChooser("selectItems", currentCorpora)
+        corpusChooserInstance.corpusChooser("selectItems", currentCorpora)
     })
 })
-
 
 korpApp.controller("headerCtrl", function($scope, $location, $uibModal, utils) {
     const s = $scope
 
     s.logoClick = function() {
         window.location = $scope.getUrl(currentMode)
-        return window.location.reload()
+        window.location.reload()
     }
 
+    s.citeClick = () => {
+        s.show_modal = "about"
+    }
 
-    s.citeClick = () => {s.show_modal = 'about'}
-
-    s.showLogin = () => {s.show_modal = 'login'}
+    s.showLogin = () => {
+        s.show_modal = "login"
+    }
 
     s.logout = function() {
         let corpus
@@ -195,16 +197,18 @@ korpApp.controller("headerCtrl", function($scope, $location, $uibModal, utils) {
         $.jStorage.deleteKey("creds")
 
         // TODO figure out another way to do this
-        for (let corpusObj of Array.from(settings.corpusListing.corpora)) {
+        for (let corpusObj of settings.corpusListing.corpora) {
             corpus = corpusObj.id
             if (corpusObj.limitedAccess) {
-                $(`#hpcorpus_${corpus}`).closest(".boxdiv").addClass("disabled")
+                $(`#hpcorpus_${corpus}`)
+                    .closest(".boxdiv")
+                    .addClass("disabled")
             }
         }
         $("#corpusbox").corpusChooser("updateAllStates")
 
         let newCorpora = []
-        for (corpus of Array.from(settings.corpusListing.getSelectedCorpora())) {
+        for (corpus of settings.corpusListing.getSelectedCorpora()) {
             if (!settings.corpora[corpus].limitedAccess) {
                 newCorpora.push(corpus)
             }
@@ -225,17 +229,16 @@ korpApp.controller("headerCtrl", function($scope, $location, $uibModal, utils) {
         s.modes = _.filter(settings.modeConfig, item => item.labOnly !== true)
     }
 
-
     s.visible = s.modes.slice(0, N_VISIBLE)
     s.menu = s.modes.slice(N_VISIBLE)
 
-    const i = $.inArray(currentMode, (_.map(s.menu, "mode")))
+    const i = _.map(s.menu, "mode").indexOf(currentMode)
     if (i !== -1) {
         s.visible.push(s.menu[i])
         s.menu.splice(i, 1)
     }
 
-    for (let mode of Array.from(s.modes)) {
+    for (let mode of s.modes) {
         mode.selected = false
         if (mode.mode === currentMode) {
             mode.selected = true
@@ -253,22 +256,23 @@ korpApp.controller("headerCtrl", function($scope, $location, $uibModal, utils) {
     s.show_modal = false
 
     let modal = null
-    utils.setupHash(s, [{
-        key: "display",
-        scope_name: "show_modal",
-        post_change(val) {
-            c.log("post change", val)
-            if (val) {
-                return showModal(val)
-            } else {
-                c.log("post change modal", modal)
-                if (modal != null) {
-                    modal.close()
+    utils.setupHash(s, [
+        {
+            key: "display",
+            scope_name: "show_modal",
+            post_change(val) {
+                c.log("post change", val)
+                if (val) {
+                    showModal(val)
+                } else {
+                    c.log("post change modal", modal)
+                    if (modal != null) {
+                        modal.close()
+                    }
+                    modal = null
                 }
-                modal = null
             }
         }
-    }
     ])
 
     const closeModals = function() {
@@ -277,18 +281,18 @@ korpApp.controller("headerCtrl", function($scope, $location, $uibModal, utils) {
     }
 
     var showModal = function(key) {
-        const tmpl = { about: require('../markup/about.html'), login: 'login_modal' }[key]
+        const tmpl = { about: require("../markup/about.html"), login: "login_modal" }[key]
         const params = {
-            templateUrl : tmpl,
-            scope : s,
-            windowClass : key
+            templateUrl: tmpl,
+            scope: s,
+            windowClass: key
         }
-        if (key === 'login') {
-            params.size = 'sm'
+        if (key === "login") {
+            params.size = "sm"
         }
         modal = $uibModal.open(params)
 
-        return modal.result.then(() => closeModals(), () => closeModals())
+        modal.result.then(() => closeModals(), () => closeModals())
     }
 
     s.clickX = () => closeModals()
@@ -302,22 +306,24 @@ korpApp.controller("headerCtrl", function($scope, $location, $uibModal, utils) {
     }
     s.loginSubmit = function(usr, pass, saveLogin) {
         s.login_err = false
-        return authenticationProxy.makeRequest(usr, pass, saveLogin).done(function(data) {
-            util.setLogin()
-            return safeApply(s, function() {
-                s.show_modal = null
-                s.restorePreLoginState()
-                s.loggedIn = true
-                s.username = usr
-        })
-        }).fail(function() {
-            c.log("login fail")
-            safeApply(s, () => {s.login_err = true})
-        })
+        authenticationProxy
+            .makeRequest(usr, pass, saveLogin)
+            .done(function(data) {
+                util.setLogin()
+                safeApply(s, function() {
+                    s.show_modal = null
+                    s.restorePreLoginState()
+                    s.loggedIn = true
+                    s.username = usr
+                })
+            })
+            .fail(function() {
+                c.log("login fail")
+                safeApply(s, () => {
+                    s.login_err = true
+                })
+            })
     }
 })
 
-
-korpApp.filter("trust", $sce =>
-    input => $sce.trustAsHtml(input)
-)
+korpApp.filter("trust", $sce => input => $sce.trustAsHtml(input))
