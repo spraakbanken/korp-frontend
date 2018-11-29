230408e4e0e8d26fd8dc8fdb4a6719b2fcf57281
Johan Roxendal <johan.roxendal@gu.se>
2018-11-29 13:44:49 +0100

    prettier kwic_download

--- app/scripts/kwic_download.js
+++ app/scripts/kwic_download.js
@@ -1,3 +1,4 @@
+/** @format */
 /* eslint-disable
     no-tabs,
     no-undef,
@@ -16,10 +17,13 @@
 
 const korpApp = angular.module("korpApp")
 korpApp.factory("kwicDownload", function() {
-
     const emptyRow = function(length) {
         const a = new Array(length)
-        for (let x = 0, end = a.length - 1, asc = end >= 0; asc ? x <= end : x >= end; asc ? x++ : x--) {
+        for (
+            let x = 0, end = a.length - 1, asc = end >= 0;
+            asc ? x <= end : x >= end;
+            asc ? x++ : x--
+        ) {
             a[x] = ""
         }
         return a
@@ -28,7 +32,9 @@ korpApp.factory("kwicDownload", function() {
     const createFile = function(dataType, fileType, content) {
         const date = moment().format("YYYYDDMM_HHmmss")
         const filename = `korp_${dataType}_${date}.${fileType}`
-        const blobURL = window.URL.createObjectURL(new Blob([content], { type: `text/${fileType}` }))
+        const blobURL = window.URL.createObjectURL(
+            new Blob([content], { type: `text/${fileType}` })
+        )
         return [filename, blobURL]
     }
 
@@ -38,7 +44,6 @@ korpApp.factory("kwicDownload", function() {
             newRow[0] = row
             return newRow
         })
-    
 
     const createSearchInfo = function(requestInfo, totalHits) {
         const rows = []
@@ -74,24 +79,31 @@ korpApp.factory("kwicDownload", function() {
     }
 
     const transformDataToAnnotations = function(data, searchInfo) {
-        const headers = _.filter(_.keys(data[1].tokens[0]),val => (val.indexOf("_") !== 0) && (val !== "structs") && (val !== "$$hashKey") && (val !== "position"))
+        const headers = _.filter(
+            _.keys(data[1].tokens[0]),
+            val =>
+                val.indexOf("_") !== 0 &&
+                val !== "structs" &&
+                val !== "$$hashKey" &&
+                val !== "position"
+        )
         const columnCount = headers.length + 1
         const res = padRows(searchInfo, columnCount)
-        res.push(["match"].concat(headers)) 
+        res.push(["match"].concat(headers))
         for (let row of Array.from(data)) {
             if (row.tokens) {
                 const textAttributes = []
-                for (let attrName  in row.structs) {
+                for (let attrName in row.structs) {
                     const attrValue = row.structs[attrName]
-                    textAttributes.push((attrName + ": \"" + attrValue + "\""))
+                    textAttributes.push(attrName + ': "' + attrValue + '"')
                 }
                 const hitInfo = emptyRow(columnCount)
                 hitInfo[0] = `# ${corpus}; text attributes: ${textAttributes.join(", ")}`
                 res.push(hitInfo)
-                
+
                 for (let token of Array.from(row.tokens || [])) {
                     var match
-                    if ((token.position >= row.match.start) && (token.position < row.match.end)) {
+                    if (token.position >= row.match.start && token.position < row.match.end) {
                         match = "***"
                     } else {
                         match = ""
@@ -106,7 +118,7 @@ korpApp.factory("kwicDownload", function() {
                 var corpus = row.newCorpus
             }
         }
-        
+
         return res
     }
 
@@ -116,7 +128,6 @@ korpApp.factory("kwicDownload", function() {
         let res = []
         for (row of Array.from(data)) {
             if (row.tokens) {
-
                 var attrName, token
                 const leftContext = []
                 for (token of Array.from(row.tokens.slice(0, row.match.start))) {
@@ -130,9 +141,9 @@ korpApp.factory("kwicDownload", function() {
                 for (token of Array.from(row.tokens.slice(row.match.end, row.tokens.length))) {
                     rightContext.push(token.word)
                 }
-                
+
                 const structs = []
-                for (attrName  in row.structs) {
+                for (attrName in row.structs) {
                     if (!Array.from(structHeaders).includes(attrName)) {
                         structHeaders.push(attrName)
                     }
@@ -145,14 +156,26 @@ korpApp.factory("kwicDownload", function() {
                     }
                 }
 
-                const newRow = [corpus, row.match.position, leftContext.join(" "), match.join(" "), rightContext.join(" ")].concat(structs)
+                const newRow = [
+                    corpus,
+                    row.match.position,
+                    leftContext.join(" "),
+                    match.join(" "),
+                    rightContext.join(" ")
+                ].concat(structs)
                 res.push(newRow)
             } else if (row.newCorpus) {
                 var corpus = row.newCorpus
             }
         }
 
-        const headers = ["corpus", "match_position", "left context", "match", "right_context"].concat(structHeaders)
+        const headers = [
+            "corpus",
+            "match_position",
+            "left context",
+            "match",
+            "right_context"
+        ].concat(structHeaders)
         res = [headers].concat(res)
 
         res.push(emptyRow(headers.length))
@@ -172,16 +195,16 @@ korpApp.factory("kwicDownload", function() {
             return transformDataToKWIC(data, searchInfo)
         }
     }
-    
+
     const makeContent = function(fileType, transformedData) {
         let dataDelimiter
         if (fileType === "csv") {
             dataDelimiter = ","
         }
         if (fileType === "tsv") {
-            dataDelimiter = "	"
+            dataDelimiter = "   "
         }
-        
+
         const csv = new CSV(transformedData, {
             delimiter: dataDelimiter
         })
