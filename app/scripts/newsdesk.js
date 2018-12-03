ec8d464146379f71e7055e9332e09fd58fe50e4e
Johan Roxendal <johan.roxendal@gu.se>
2018-12-03 09:14:24 +0100

    prettified and completed newsdesk

--- app/scripts/newsdesk.js
+++ app/scripts/newsdesk.js
@@ -1,12 +1,10 @@
+/** @format */
 /* eslint-disable
-    no-return-assign,
-    no-undef,
 */
 // TODO: This file was created by bulk-decaffeinate.
 // Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
  * DS102: Remove unnecessary code created because of implicit returns
  * DS205: Consider reworking code to avoid use of IIFEs
  * DS207: Consider shorter variations of null checks
@@ -15,35 +13,38 @@
 // SB-newsdesk 1.0b
 // Requirements: JQuery, JQuery.ui.position, trust filter, loc filter, Font Awesome
 
-angular.module('newsdesk', []).directive("newsDesk", ($window, $document, $rootElement, $http, $location) =>
-    ({
-        template : `\
-<div>
-        <div ng-if="shouldUseThis" class="newsdesk-opener" ng-click="togglePopover($event)" ng-class="{'newsdesk-new-news': numNewNews != 0, 'newsdesk-no-new-news' : numNewNews == 0}">
-            <i class="fa fa-bell newsdesk-bell"></i>
-            <div class="newsdesk-arrow-box">
-                <span>{{numNewNews}}</span>
-            </div>&nbsp;
-        </div>
-        <div class="popover newsdesk-popover" ng-click="onPopoverClick($event)" to-body>
-            <div class="arrow"></div>
-            <h2 class="popover-title">{{header | loc:lang}}<span style="float:right;cursor:pointer" ng-click="popHide()">×</span></h2>
-            <div class="newsdesk-around-items">
-                <div class="newsdesk-news-item" ng-repeat="item in newsitems" ng-class="{'newsdesk-new-news-item': (item.d > lastChecked)}">
-                    <h4>{{item.t[currentLang]}}</h4>
-                    <span class="newsdesk-item-date">{{item.d}}</span>
-                    <div ng-bind-html="item.h[currentLang] | trust"></div>
+angular
+    .module("newsdesk", [])
+    .directive("newsDesk", ($window, $document, $rootElement, $http, $location) => ({
+        template: `
+            <div>
+                <div ng-if="shouldUseThis" class="newsdesk-opener" 
+                     ng-click="togglePopover($event)" 
+                     ng-class="{'newsdesk-new-news': numNewNews != 0, 'newsdesk-no-new-news' : numNewNews == 0}">
+                    <i class="fa fa-bell newsdesk-bell"></i>
+                    <div class="newsdesk-arrow-box">
+                        <span>{{numNewNews}}</span>
+                    </div>&nbsp;
                 </div>
-            </div>
-        </div>
-</div>\
-`,
-        restrict : "EA",
-        replace : true,
-        scope : { "header" : "=", "storage" : "=" },
+                <div class="popover newsdesk-popover" ng-click="onPopoverClick($event)" to-body>
+                    <div class="arrow"></div>
+                    <h2 class="popover-title">{{header | loc:lang}}<span style="float:right;cursor:pointer" ng-click="popHide()">×</span></h2>
+                    <div class="newsdesk-around-items">
+                        <div class="newsdesk-news-item" ng-repeat="item in newsitems" 
+                             ng-class="{'newsdesk-new-news-item': (item.d > lastChecked)}">
+                            <h4>{{item.t[currentLang]}}</h4>
+                            <span class="newsdesk-item-date">{{item.d}}</span>
+                            <div ng-bind-html="item.h[currentLang] | trust"></div>
+                        </div>
+                    </div>
+                </div>
+        </div>`,
+        restrict: "EA",
+        replace: true,
+        scope: { header: "=", storage: "=" },
         link(scope, elem, attr) {
             const s = scope
-            s.shouldUseThis = (settings.newsDeskUrl != null)
+            s.shouldUseThis = settings.newsDeskUrl != null
 
             if (!s.shouldUseThis) {
                 return
@@ -68,16 +69,12 @@ angular.module('newsdesk', []).directive("newsDesk", ($window, $document, $rootE
                     contentType: "application/json",
                     dataType: "jsonp",
                     success(json) {
-                        const currentDate = new (Date().toISOString().slice(0, 10))()
-                        s.newsitems = ((() => {
-                            const result = []
-                            for (let newsitem of Array.from(json)) {
-                                 if (((newsitem.e == null)) || (newsitem.e >= currentDate)) {
-                                    result.push(newsitem)
-                                }
-                            }
-                            return result
-                        })())
+                        const currentDate = new (Date()
+                            .toISOString()
+                            .slice(0, 10))()
+                        s.newsitems = _.filter(json, newsitem => {
+                            return !newsitem.e || newsitem.e >= currentDate
+                        })
                         let n = 0
                         for (let nItem of Array.from(s.newsitems)) {
                             if (nItem.d > s.lastChecked) {
@@ -85,12 +82,12 @@ angular.module('newsdesk', []).directive("newsDesk", ($window, $document, $rootE
                             }
                         }
 
-                        return safeApply(s, () => s.numNewNews = n)
+                        safeApply(s, () => (s.numNewNews = n))
                     },
 
                     error(e) {
-                       return console.log("error, couldn't fetch news", e.message)
-                   }
+                        console.log("error, couldn't fetch news", e.message)
+                    }
                 })
             }
 
@@ -108,7 +105,7 @@ angular.module('newsdesk', []).directive("newsDesk", ($window, $document, $rootE
                     s.numNewNews = 0
                 }
                 event.preventDefault()
-                return event.stopPropagation()
+                event.stopPropagation()
             }
 
             const popover = $(".newsdesk-popover")
@@ -124,23 +121,25 @@ angular.module('newsdesk', []).directive("newsDesk", ($window, $document, $rootE
             s.popShow = function() {
                 s.isPopoverVisible = true
 
-                popover.show().focus().position({
-                    my : "right top",
-                    at : "right-10 top+10",
-                    of : window
-                })
+                popover
+                    .show()
+                    .focus()
+                    .position({
+                        my: "right top",
+                        at: "right-10 top+10",
+                        of: window
+                    })
                 $rootElement.on("keydown", handleEscape)
                 $rootElement.on("click", s.popHide)
 
-                return localStorage.setItem(s.storage, s.newsitems[0].d)
+                localStorage.setItem(s.storage, s.newsitems[0].d)
             }
 
-            return s.popHide = function() {
+            s.popHide = function() {
                 s.isPopoverVisible = false
                 popover.hide()
                 $rootElement.off("keydown", handleEscape)
                 $rootElement.off("click", s.popHide)
             }
         }
-    })
-)
+    }))
