fcd34e2939e51eea940512828bd37efd2c48966d
Maria Öhrman <maria.ohrman@gu.se>
2019-09-19 16:35:57 +0200

    not needed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,25 +1,21 @@
-const merge = require('webpack-merge');
-// const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
-const common = require('./webpack.common.js');
+/** @format */
+const merge = require("webpack-merge")
+const common = require("./webpack.common.js")
 const CompressionPlugin = require("compression-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = merge(common, {
-  plugins: [
-    new CompressionPlugin({
-      // test: ""
-    })
-  ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          mangle: {
-            reserved: ['$super'],
-          },
-        }
-      })
-    ]
-  },
-  mode: 'production'
-});
+    plugins: [new CompressionPlugin({})],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    mangle: {
+                        reserved: ["$super"]
+                    }
+                }
+            })
+        ]
+    },
+    mode: "production"
+})
