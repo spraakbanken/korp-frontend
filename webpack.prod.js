a0bf60fb233fa98a87bf7ce99e2dcf08ef8e1c8a
Johan Roxendal <johan.roxendal@gu.se>
2020-08-06 13:42:52 +0200

    fixed build command

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 /** @format */
-const merge = require("webpack-merge")
+const {merge} = require("webpack-merge")
 const common = require("./webpack.common.js")
 const CompressionPlugin = require("compression-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
