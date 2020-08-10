01fd1dd19ba2aeaf063c2831093175ded0a07054
Johan Roxendal <johan.roxendal@gu.se>
2020-08-10 08:57:56 +0200

    added missing 'from' loc value.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+      "./app/**/*.js", 
+      "./app/**/*.html",
+      "./app/**/*.pug"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
