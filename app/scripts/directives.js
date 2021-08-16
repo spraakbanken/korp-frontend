spraakbanken/korp-frontend

commit cd537ca5072c8540dfaab3592ac17322254fe020

add new states and actions in statemachine, use for links in sidebar

app/scripts/directives.js
@@ -468,6 +468,9 @@ korpApp.directive("extendedList", () => ({
         setCQP(s.cqp)
 
         s.$watch("data", () => (s.cqp = CQP.stringify(s.data) || ""), true)
+        s.$watch("cqp", () => {
+            setCQP(s.cqp)
+        })
 
         s.addOr = function (and_array) {
             let last = _.last(and_array) || {}

app/scripts/search_controllers.js
@@ -414,8 +414,8 @@ korpApp.controller("ExtendedSearch", function (
         compareSearches.saveSearch(name, $rootScope.extendedCQP)
     )
 
-    s.searches = searches
-    s.$on("btn_submit", function () {
+    // TODO this is *too* weird
+    function triggerSearch() {
         $location.search("search", null)
         $location.search("page", null)
         $location.search("in_order", null)
@@ -426,6 +426,17 @@ korpApp.controller("ExtendedSearch", function (
             }
             $location.search("within", within)
         }, 0)
+    }
+
+    statemachine.listen("cqp_search", (event) => {
+        $scope.$root.searchtabs()[1].tab.select()
+        s.cqp = event.cqp
+        triggerSearch()
+    })
+
+    s.searches = searches
+    s.$on("btn_submit", function () {
+        triggerSearch()
     })
 
     s.$on("extended_set", ($event, val) => (s.cqp = val))

app/scripts/statemachine.js
@@ -58,6 +58,21 @@ let machine = Machine(
                     },
                 },
             },
+            extended_search: {
+                initial: "blank",
+                states: {
+                    blank: {},
+                    cqp_selected: {
+                        entry: "log",
+                    }
+                },
+                on: {
+                    SEARCH_CQP: {
+                        target: "extended_search.cqp_selected",
+                        actions: ["cqp_search"],
+                    },
+                }
+            },
             sidebar: {
                 ...sidebarStates,
             },
@@ -74,6 +89,7 @@ let machine = Machine(
             select_word: (context, event) => broadcast("select_word", event),
             deselect_word: (context, event) => broadcast("select_word", null),
             lemgram_search: (context, event) => broadcast("lemgram_search", event),
+            cqp_search: (context, event) => broadcast("cqp_search", event),
         },
     }
 )

commit 2c5efafe7a5e2e1b9d1b72f3b20c71994cb6d5a7

fix #178 focus lost for each keydown
This error was introduced in cd537ca add new states and actions in
statemachine.

app/scripts/directives.js
@@ -432,7 +432,9 @@ korpApp.directive("extendedList", () => ({
         const s = $scope
 
         const setCQP = function (val) {
-            s.data = CQP.parse(val)
+            if (s.data == undefined || val != CQP.stringify(s.data)) {
+                s.data = CQP.parse(val)
+            }
         }
 
         if (s.cqp == null) {