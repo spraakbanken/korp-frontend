c1d0557579a60d9b522058d78bb451466089c927
Maria Öhrman <maria.ohrman@gu.se>
2022-06-13 11:21:53 +0200

    move thing from webpack config to babel config

--- webpack.common.js
+++ webpack.common.js
@@ -40,9 +40,6 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
                 },
             },
             {
