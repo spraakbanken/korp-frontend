65e94eb8f62c69191ce2227165aa796bb5d19a7a
Maria Öhrman <maria.ohrman@gu.se>
2023-02-10 14:17:50 +0100

    remove babel

--- webpack.common.js
+++ webpack.common.js
@@ -36,13 +36,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
                 test: /\.tsx?$/,
                 use: {
                     loader: "ts-loader",
