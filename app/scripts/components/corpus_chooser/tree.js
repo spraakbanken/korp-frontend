283d6250e70015844534b631bff3194640945d92
Maria Öhrman <maria.ohrman@gu.se>
2023-04-04 08:57:22 +0200

    fix #306 corpus chooser long folder/corpus names

--- app/scripts/components/corpus_chooser/tree.js
+++ app/scripts/components/corpus_chooser/tree.js
@@ -10,33 +10,36 @@ export const ccTreeComponent = {
             class="tree relative"
             ng-class="{ collapsed: !folder.extended, extended: folder.extended, disabled: folder['limited_access'] }"
             >
-            <img ng-click="$ctrl.toggleFolderVisibility(folder)" src="${collapsedImg}" alt="extend" class="ext cursor-pointer">
-            <label 
-                class="boxlabel cursor-pointer"
-                ng-click="$ctrl.toggleFolderSelection($event, folder)">
-                <span class="checkbox" ng-class="{ checked: folder.selected == 'all', unchecked: folder.selected == 'none', intermediate: folder.selected == 'some' }"></span>
-                <span>{{ folder.title | locObj:$root.lang }}</span>
-                <span class="numberOfChildren">({{folder.numberOfChildren}})</span>
-            </label>
-            <i 
-                ng-click="$ctrl.showInfo($event, folder)" 
-                class="fa-solid text-xl fa-info-circle text-gray-700 mt-1 rounded-full bg-white text-blue-500 absolute right-2"></i>
+            <div class="flex">
+                <img ng-click="$ctrl.toggleFolderVisibility(folder)" src="${collapsedImg}" alt="extend" class="ext cursor-pointer self-start mt-2">
+                <label
+                    class="flex-1 boxlabel cursor-pointer"
+                    ng-click="$ctrl.toggleFolderSelection($event, folder)">
+                    <span class="inline-block checkbox mr-1" ng-class="{ checked: folder.selected == 'all', unchecked: folder.selected == 'none', intermediate: folder.selected == 'some' }"></span>
+                    <span>{{ folder.title | locObj:$root.lang }}</span>
+                    <span class="numberOfChildren">({{folder.numberOfChildren}})</span>
+                </label>
+                <i
+                    ng-click="$ctrl.showInfo($event, folder)"
+                    class="fa-solid text-xl fa-info-circle text-gray-700 rounded-full bg-white text-blue-500 mr-1 mt-1 h-fit"></i>
+            </div>
             <cc-tree ng-if="folder.extended" node="folder" indent="true" on-select="$ctrl.onChildSelect()" on-select-only="$ctrl.selectOnly(corporaIds)" on-show-info="$ctrl.onShowInfoLocal(node)"></cc-tree>
         </div>
         <div ng-repeat="corpus in $ctrl.sortedCorpora"
-            class="boxdiv relative"
-            ng-class="{ 'disabled cursor-default': !corpus.userHasAccess }"
-            style="margin-left:16px; background-color: rgb(221, 233, 255)"
+            class="boxdiv flex"
+            ng-class="{ 'disabled cursor-default': !corpus.userHasAccess, 'cursor-pointer': corpus.userHasAccess }"
+            style="margin-left:16px; background-color: rgb(221, 233, 255); margin-bottom: 2px; padding-top: 1px; padding-bottom: 1px;"
             ng-click="$ctrl.toggleCorpusSelection($event, corpus)"
             >
-            <label class="px-2" 'cursor-pointer': corpus.userHasAccess }>
-                <span class="checkbox" ng-class="{ checked: corpus.selected, unchecked: !corpus.selected }"></span>
-                <span>{{ corpus.title | locObj:$root.lang }}</span>
+            <span class="mx-1 my-1 checkbox" ng-class="{ checked: corpus.selected, unchecked: !corpus.selected }"></span>
+            <label class="px-1 flex-1">
+                {{ corpus.title | locObj:$root.lang }}
             </label>
-            <i ng-if="corpus['limited_access'] && corpus.userHasAccess" class="fa-solid fa-unlock"></i>
-            <i 
+            <i ng-if="corpus['limited_access'] && corpus.userHasAccess" class="fa-solid fa-unlock mx-1 my-1"></i>
+            <i
                 ng-click="$ctrl.showInfo($event, corpus)" 
-                class="fa-solid text-xl fa-info-circle text-gray-700 mt-1 rounded-full bg-white text-blue-500 absolute right-2"></i>
+                style="margin-top: 2px;"
+                class="fa-solid text-xl fa-info-circle text-gray-700 rounded-full bg-white text-blue-500 mr-1 h-fit"></i>
         </div>
     </div>
     `,
