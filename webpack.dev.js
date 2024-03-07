5f756bacfd96f6b2fb4ec3cb7753460b18bb0d9b
Arild Matsson <arild.matsson@gu.se>
2024-03-07 15:03:49 +0100

    Fix hot reloading with optimization.runtimeChunk

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,29 +1,34 @@
-const {merge} = require('webpack-merge');
-const common = require('./webpack.common.js');
+/** @format */
+const { merge } = require("webpack-merge")
+const common = require("./webpack.common.js")
 
 const host = process.env.KORP_HOST || "localhost"
 const port = process.env.KORP_PORT || 9111
 
 let server
 if (process.env.KORP_HTTPS) {
-  server = {
-    type: "https",
-    options: {
-      key: process.env.KORP_KEY,
-      cert: process.env.KORP_CERT,
+    server = {
+        type: "https",
+        options: {
+            key: process.env.KORP_KEY,
+            cert: process.env.KORP_CERT,
+        },
     }
-  }
 } else {
-  https = false
-  server = "http"
+    https = false
+    server = "http"
 }
 
 module.exports = merge(common, {
-  devServer: {
-    host,
-    port,
-    server
-  },
-  devtool: 'inline-source-map',
-  mode: 'development'
-});
+    devServer: {
+        host,
+        port,
+        server,
+    },
+    devtool: "inline-source-map",
+    optimization: {
+        // Needed for hot reloading with multiple entrypoints
+        runtimeChunk: "single",
+    },
+    mode: "development",
+})
