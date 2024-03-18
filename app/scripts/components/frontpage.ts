039e10766aeb41deb23834069994fc16a88d0378
Arild Matsson <arild.matsson@gu.se>
2024-03-18 15:00:29 +0100

    Expandable corpus updates list

--- app/scripts/components/frontpage.ts
+++ app/scripts/components/frontpage.ts
@@ -1,5 +1,6 @@
 /** @format */
 import angular from "angular"
+import moment from "moment"
 
 export default angular.module("korpApp").component("frontpage", {
     template: /* HTML */ `
@@ -8,14 +9,27 @@ export default angular.module("korpApp").component("frontpage", {
                 <div ng-bind-html="$root._settings['description'] | locObj:lang | trust"></div>
             </div>
 
-            <section ng-if="$ctrl.recentUpdates" class="w-80 grow">
+            <section ng-if="$ctrl.recentUpdates && $ctrl.recentUpdates.length" class="w-80 grow">
                 <h2 class="text-xl font-bold">{{"front_corpus_updates" | loc:$root.lang}}</h2>
                 <div class="my-2 flex flex-col gap-2">
-                    <article ng-repeat="corpus in $ctrl.recentUpdates" class="flex-1">
+                    <article ng-repeat="corpus in $ctrl.recentUpdatesFiltered" class="flex-1">
                         <strong>{{::corpus.info.Updated}}</strong>
                         <em>{{corpus.title | locObj:$root.lang}}</em>
                         {{"front_corpus_updated" | loc:$root.lang}}.
                     </article>
+
+                    <div ng-if="$ctrl.recentUpdates.length > $ctrl.RECENT_UPDATES_LIMIT">
+                        <a ng-if="!$ctrl.recentUpdatesExpanded" ng-click="$ctrl.toggleRecentUpdatesExpanded()">
+                                <i class="fa fa-angle-double-down"></i>
+                                {{"show_more" | loc:$root.lang}}
+                            </a>
+                        </span>
+                        <a ng-if="$ctrl.recentUpdatesExpanded" ng-click="$ctrl.toggleRecentUpdatesExpanded()">
+                                <i class="fa fa-angle-double-up"></i>
+                                {{"show_less" | loc:$root.lang}}
+                            </a>
+                        </span>
+                    </div>
                 </div>
             </section>
         </div>
@@ -26,8 +40,11 @@ export default angular.module("korpApp").component("frontpage", {
         "searches",
         function ($rootScope, searches) {
             const $ctrl = this
+            $ctrl.RECENT_UPDATES_LIMIT = 5
             $ctrl.showDescription = false
             $ctrl.recentUpdates = null
+            $ctrl.recentUpdatesExpanded = false
+            $ctrl.recentUpdatesFiltered = null
 
             $ctrl.hasResult = () =>
                 searches.activeSearch ||
@@ -40,12 +57,20 @@ export default angular.module("korpApp").component("frontpage", {
 
             $ctrl.$onInit = () => {
                 if ($rootScope._settings.frontpage?.corpus_updates) {
+                    const limitDate = moment().subtract(6, "months")
                     // Find most recently updated corpora
                     $ctrl.recentUpdates = (window as any).settings.corpusListing.corpora
-                        .filter((corpus) => corpus.info.Updated)
+                        .filter((corpus) => corpus.info.Updated && moment(corpus.info.Updated).isSameOrAfter(limitDate))
                         .sort((a, b) => b.info.Updated.localeCompare(a.info.Updated))
-                        .slice(0, 5)
+                    $ctrl.toggleRecentUpdatesExpanded(false)
                 }
+            }
+
+            $ctrl.toggleRecentUpdatesExpanded = (to?: boolean) => {
+                $ctrl.recentUpdatesExpanded = to !== undefined ? to : !$ctrl.recentUpdatesExpanded
+                $ctrl.recentUpdatesFiltered = $ctrl.recentUpdatesExpanded
+                    ? $ctrl.recentUpdates
+                    : $ctrl.recentUpdates.slice(0, $ctrl.RECENT_UPDATES_LIMIT)
             }
         },
     ],
