d88e0ed6a79cd24aafca6ef391738d1f89e55f33
Arild Matsson <arild.matsson@gu.se>
2024-04-30 13:51:23 +0200

    Frontpage layout: examples next to description

--- app/scripts/components/frontpage.ts
+++ app/scripts/components/frontpage.ts
@@ -9,32 +9,34 @@ import "@/components/newsdesk"
 
 export default angular.module("korpApp").component("frontpage", {
     template: html`
-        <div ng-if="!$ctrl.hasResult()" class="max-w-screen-md my-4 mx-auto flex gap-4 flex-wrap">
-            <section
-                ng-if="$ctrl.showDescription && ($root._settings['description'] || $root._settings['mode_description'])"
-                class="text-lg"
-            >
-                <div
-                    ng-if="$root._settings['description']"
-                    ng-bind-html="$root._settings['description'] | locObj:lang | trust"
-                />
-                <div
-                    ng-if="$root._settings['mode_description']"
-                    ng-bind-html="$root._settings['mode_description'] | locObj:lang | trust"
-                />
-            </section>
+        <div ng-if="!$ctrl.hasResult()" class="max-w-screen-md my-10 mx-auto flex gap-8 flex-wrap">
+            <div class="w-full flex gap-8 flex-wrap">
+                <section
+                    ng-if="$ctrl.showDescription && ($root._settings['description'] || $root._settings['mode_description'])"
+                    class="w-80 grow text-lg"
+                >
+                    <div
+                        ng-if="$root._settings['description']"
+                        ng-bind-html="$root._settings['description'] | locObj:lang | trust"
+                    />
+                    <div
+                        ng-if="$root._settings['mode_description']"
+                        ng-bind-html="$root._settings['mode_description'] | locObj:lang | trust"
+                    />
+                </section>
 
-            <section ng-if="$ctrl.examples" class="w-80 grow">
-                <h2 class="text-xl font-bold">{{"example_queries" | loc:$root.lang}}</h2>
-                <ul class="my-2 list-disc">
-                    <li ng-repeat="example in $ctrl.examples" class="ml-6 mt-2 first_mt-0">
-                        <a ng-click="$ctrl.setSearch(example.params)"> {{example.label | locObj:$root.lang}} </a>
-                        <span ng-if="example.hint" class="italic">
-                            – <span ng-bind-html="example.hint | locObj:$root.lang | trust" />
-                        </span>
-                    </li>
-                </ul>
-            </section>
+                <section ng-if="$ctrl.examples" class="w-80 grow">
+                    <h2 class="text-xl font-bold">{{"example_queries" | loc:$root.lang}}</h2>
+                    <ul class="my-2 list-disc">
+                        <li ng-repeat="example in $ctrl.examples" class="ml-6 mt-2 first_mt-0">
+                            <a ng-click="$ctrl.setSearch(example.params)"> {{example.label | locObj:$root.lang}} </a>
+                            <span ng-if="example.hint" class="italic">
+                                – <span ng-bind-html="example.hint | locObj:$root.lang | trust" />
+                            </span>
+                        </li>
+                    </ul>
+                </section>
+            </div>
 
             <newsdesk class="w-80 grow"></newsdesk>
 
