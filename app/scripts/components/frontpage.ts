ca31c3aeaf8ef5c870a8d5ee1e5f27392aed6d3e
Arild Matsson <arild.matsson@gu.se>
2024-04-30 13:51:23 +0200

    Frontpage layout: examples next to description

--- app/scripts/components/frontpage.ts
+++ app/scripts/components/frontpage.ts
@@ -7,22 +7,24 @@ import "@/components/search-examples"
 
 export default angular.module("korpApp").component("frontpage", {
     template: html`
-        <div ng-if="!$ctrl.hasResult()" class="max-w-screen-md my-4 mx-auto flex gap-4 flex-wrap">
-            <section
-                ng-if="$ctrl.showDescription && ($root._settings['description'] || $root._settings['mode_description'])"
-                class="text-lg"
-            >
-                <div
-                    ng-if="$root._settings['description']"
-                    ng-bind-html="$root._settings['description'] | locObj:lang | trust"
-                />
-                <div
-                    ng-if="$root._settings['mode_description']"
-                    ng-bind-html="$root._settings['mode_description'] | locObj:lang | trust"
-                />
-            </section>
+        <div ng-if="!$ctrl.hasResult()" class="max-w-screen-md my-10 mx-auto flex gap-8 flex-wrap">
+            <div class="w-full flex gap-8 flex-wrap">
+                <section
+                    ng-if="$ctrl.showDescription && ($root._settings['description'] || $root._settings['mode_description'])"
+                    class="w-80 grow text-lg"
+                >
+                    <div
+                        ng-if="$root._settings['description']"
+                        ng-bind-html="$root._settings['description'] | locObj:lang | trust"
+                    />
+                    <div
+                        ng-if="$root._settings['mode_description']"
+                        ng-bind-html="$root._settings['mode_description'] | locObj:lang | trust"
+                    />
+                </section>
 
-            <search-examples class="w-80 grow"></search-examples>
+                <search-examples class="w-80 grow"></search-examples>
+            </div>
 
             <newsdesk class="w-80 grow"></newsdesk>
 
