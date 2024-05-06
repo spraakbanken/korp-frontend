a5b1c8ea6a209d1db994af6f48a55cd2c4da75b2
Arild Matsson <arild.matsson@gu.se>
2024-05-06 16:22:08 +0200

    Fix mode_description display, add heading

--- app/scripts/components/frontpage.ts
+++ app/scripts/components/frontpage.ts
@@ -16,11 +16,14 @@ export default angular.module("korpApp").component("frontpage", {
                     <div
                         ng-if="$root._settings['description']"
                         ng-bind-html="$root._settings['description'] | locObj:lang | trust"
-                    />
-                    <div
-                        ng-if="$root._settings['mode_description']"
-                        ng-bind-html="$root._settings['mode_description'] | locObj:lang | trust"
-                    />
+                    ></div>
+
+                    <div ng-if="$root._settings['mode_description']">
+                        <h3 ng-if="$root._settings['mode']['label']" class="font-bold">
+                            {{$root._settings['mode']['label'] | locObj:lang}}
+                        </h3>
+                        <div ng-bind-html="$root._settings['mode_description'] | locObj:lang | trust"></div>
+                    </div>
                 </section>
 
                 <search-examples class="w-80 grow"></search-examples>
