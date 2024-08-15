f7ff2f1ce28ccf27b99d9755893753f4281fb075
Arild Matsson <arild.matsson@gu.se>
2024-08-15 16:45:01 +0200

    refactor(ts): directive scroll

--- app/scripts/directives/scroll.ts
+++ app/scripts/directives/scroll.ts
@@ -1,21 +1,27 @@
 /** @format */
-const korpApp = angular.module("korpApp")
+import angular, { IAttributes } from "angular"
 
-korpApp.directive("scrollToStart", function () {
+type ScrollToStartAttrs = IAttributes & {
+    scrollToStart: string
+}
+
+angular.module("korpApp").directive("scrollToStart", function () {
     return {
         restrict: "A",
-        link: function (scope, elm, attr) {
+        link: function (scope, elm, attr: ScrollToStartAttrs) {
             const pElm = elm[0].parentElement.parentElement
 
-            let isStart
-            scope.$watch(attr.scrollToStart, function (newValue) {
-                newValue = !!newValue
+            let isStart: boolean
+
+            // If the attribute value is switched from `false` to `true`, scroll to start.
+            scope.$watch(attr.scrollToStart, function (newValue: boolean) {
                 if (!isStart && newValue) {
                     pElm.scrollTo(0, 0)
                 }
                 isStart = newValue
             })
 
+            // When user scrolls away from start, reset.
             angular.element(pElm).bind("scroll", function () {
                 if (pElm.scrollLeft !== 0 && isStart) {
                     scope.$apply(function () {
