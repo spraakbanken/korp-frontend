fd6b05f92969da0cfa50e2eb480ccf65aba824b5
Arild Matsson <arild@klavaro.se>
2024-10-18 17:49:57 +0200

    Switch to ngVue3

--- app/scripts/vue.ts
+++ app/scripts/vue.ts
@@ -1,5 +1,6 @@
 /** @format */
 import angular from "angular"
+import { ngVueComponent } from "@jaredmcateer/ngvue3"
 import TabPreloader from "@/components/TabPreloader.vue"
 
-angular.module("korpApp").directive("tabPreloader", (createVueComponent) => createVueComponent(TabPreloader))
+angular.module("korpApp").directive(...ngVueComponent("tabPreloader", TabPreloader))
