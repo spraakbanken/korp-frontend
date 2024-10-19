3115b9c5caae9baeed0a91154554b763120d4642
Arild Matsson <arild@klavaro.se>
2024-10-19 10:55:33 +0200

    CorpusUpdates.vue, lang as prop

--- app/scripts/vue.ts
+++ app/scripts/vue.ts
@@ -1,6 +1,10 @@
 /** @format */
 import angular from "angular"
 import { ngVueComponent } from "@jaredmcateer/ngvue3"
+import CorpusUpdates from "@/components/CorpusUpdates.vue"
 import TabPreloader from "@/components/TabPreloader.vue"
 
-angular.module("korpApp").directive(...ngVueComponent("tabPreloader", TabPreloader))
+angular
+    .module("korpApp")
+    .directive(...ngVueComponent("corpusUpdates", CorpusUpdates))
+    .directive(...ngVueComponent("tabPreloader", TabPreloader))
