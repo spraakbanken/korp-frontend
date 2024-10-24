e0d3723595a4f8532780cfb73feddc99c0fc2dbc
Arild Matsson <arild.matsson@gu.se>
2024-10-24 13:10:05 +0200

    refactor(ts): deptree

--- app/scripts/components/deptree/deptree_deps.js
+++
@@ -1,6 +0,0 @@
-import { Visualizer } from "../../../lib/brat/client/src/visualizer.js"
-import "../../../lib/brat/style-vis.css"
-import "../../../lib/jquery.svg.js"
-import "../../../lib/jquery.svgdom.js"
-
-export default Visualizer;
