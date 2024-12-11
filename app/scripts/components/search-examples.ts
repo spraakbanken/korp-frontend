bd2aaaf1b0305cc35423d2053eabfd15621c5f09
Arild Matsson <arild.matsson@gu.se>
2024-12-11 15:02:32 +0100

    fix: search examples params patch

--- app/scripts/components/search-examples.ts
+++ app/scripts/components/search-examples.ts
@@ -44,7 +44,8 @@ export default angular.module("korpApp").component("searchExamples", {
                 if (params.cqp) {
                     statemachine.send("SEARCH_CQP", { cqp: params.cqp } as CqpSearchEvent)
                 }
-                $location.search(params)
+                // Do not use `$location.search(params)` because it will remove existing params (like `corpus`)
+                Object.keys(params).forEach((key: keyof HashParams) => $location.search(key, params[key]))
             }
         },
     ],
