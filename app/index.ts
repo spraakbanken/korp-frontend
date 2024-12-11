0f4a6f3053a6bf0d02db5e8e9c73d2ca3aaca8d6
Arild Matsson <arild.matsson@gu.se>
2024-12-11 15:30:46 +0100

    refactor: drop JQueryUI Smoothness theme

--- app/index.ts
+++ app/index.ts
@@ -29,7 +29,6 @@ require("rickshaw/rickshaw.css")
 
 require("leaflet/dist/leaflet.css")
 require("leaflet.markercluster/dist/MarkerCluster.css")
-require("components-jqueryui/themes/smoothness/jquery-ui.min.css")
 require("./styles/_bootstrap-custom.scss")
 
 require("./styles/tailwind.scss")
