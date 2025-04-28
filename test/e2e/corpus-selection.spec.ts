3519a70cf22de78cf97b6b592168b5d4559f8ecc
Arild Matsson <arild.matsson@gu.se>
2025-04-28 13:31:09 +0200

    Test more corpus selection

---
+++ test/e2e/corpus-selection.spec.ts
@@ -0,0 +1,102 @@
+import { test, expect, Page } from "@playwright/test"
+import { describe } from "node:test"
+
+describe("init corpus selection", () => {
+    test("no param", async ({ page }) => {
+        await page.goto("/#?lang=eng")
+        await expect(page.locator("corpus-chooser")).toContainText("23 of")
+    })
+
+    test("empty param", async ({ page }) => {
+        // This case is not expected, so it's not very important what it resolves to, but let's make sure there's no error.
+        await page.goto("/#?lang=eng&corpus=")
+        await expect(page.locator("corpus-chooser")).toContainText("0 of")
+    })
+
+    test("few selected", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=attasidor,da")
+        await expect(page.locator("corpus-chooser")).toContainText("2 of")
+    })
+
+    test("some nonexisting", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=attasidor,da,abc")
+        await expect(page.getByRole("dialog")).toContainText("Some selected corpora are not available")
+        await page.getByRole("button").click()
+        await expect(page.locator("corpus-chooser")).toContainText("2 of")
+    })
+
+    test("all nonexisting", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=abc,def")
+        await expect(page.getByRole("dialog")).toContainText("Some selected corpora are not available")
+        await page.getByRole("button").click()
+        await expect(page.locator("corpus-chooser")).toContainText("23 of")
+    })
+
+    test("some protected", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=attasidor,da,asu")
+        await expect(page.getByRole("dialog")).toContainText("Login needed")
+        await page.getByRole("button").click()
+        // Dismiss login prompt
+        await page.locator(".close-x").click()
+        await expect(page.locator("corpus-chooser")).toContainText("2 of")
+    })
+
+    test("all protected", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=asu")
+        await expect(page.getByRole("dialog")).toContainText("Login needed")
+        await page.getByRole("button").click()
+        // Dismiss login prompt
+        await page.locator(".close-x").click()
+        await expect(page.locator("corpus-chooser")).toContainText("23 of")
+    })
+})
+
+describe("changing corpus selection", () => {
+    let settings: any
+
+    async function checkCorpusListing(page: Page, expected: string[]) {
+        const selected = await page.evaluate(() => settings!.corpusListing.getSelectedCorpora())
+        expect(selected).toEqual(expected)
+    }
+
+    const getUrlParam = (url: string) => new URLSearchParams(new URL(url).hash.slice(1)).get("corpus")
+
+    test("corpus chooser", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=attasidor")
+        await expect(page.locator("corpus-chooser")).toBeVisible() // Wait for initialization
+        await checkCorpusListing(page, ["attasidor"])
+
+        // Add to selection
+        await page.locator("corpus-chooser").click()
+        await page.getByText("Dramawebben (demo)").click()
+        await expect(page.locator("corpus-chooser")).toContainText("2 of")
+        await checkCorpusListing(page, ["attasidor", "drama"])
+        expect(getUrlParam(page.url())).toEqual("attasidor,drama")
+
+        // Remove from selection
+        await page.getByText("Dramawebben (demo").click()
+        await expect(page.locator("corpus-chooser")).toContainText("8 Sidor selected")
+        await checkCorpusListing(page, ["attasidor"])
+        expect(getUrlParam(page.url())).toEqual("attasidor")
+    })
+
+    test("select recently updated corpus", async ({ page }) => {
+        await page.goto("/#?lang=eng&corpus=attasidor")
+        await expect(page.locator("corpus-chooser")).toBeVisible() // Wait for initialization
+
+        // Find most recently updated corpus
+        const corpus: any = await page.evaluate(() => {
+            const corpora = Object.values(settings!.corpora)
+            corpora.sort((a: any, b: any) => (b.info.Updated || '0').localeCompare(a.info.Updated || '0'))
+            return corpora[0]
+        })
+        const title = corpus.title["eng"]
+        // Click its select button on the frontpage
+        await page.getByText(`${title} was updated`).getByRole("button").click()
+
+        // Only the new corpus is selected
+        await expect(page.locator("corpus-chooser")).toContainText(`${title} selected`)
+        await checkCorpusListing(page, [corpus.id])
+        expect(getUrlParam(page.url())).toEqual(corpus.id)
+    })
+})
