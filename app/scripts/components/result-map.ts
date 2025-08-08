8668f65d74e780df4b80b395a033c80067f4fc6e
Arild Matsson <arild.matsson@gu.se>
2025-08-08 21:35:51 +0200

    refactor: extract core map code

--- app/scripts/components/result-map.ts
+++ app/scripts/components/result-map.ts
@@ -6,7 +6,20 @@ import { html } from "@/util"
 import { RootScope } from "@/root-scope.types"
 import "@/../styles/map.scss"
 import { AppSettings } from "@/settings/app-settings.types"
-import { Point } from "@/map_services"
+import {
+    createCircleMarker,
+    createMultiMarkerIcon,
+    CustomMarker,
+    CustomMarkerMany,
+    isMarker,
+    isMarkerCluster,
+    MarkerCluster,
+    MarkerClusterGroup,
+    MarkerData,
+    MarkerEvent,
+    MarkerGroup,
+    MergedMarker,
+} from "@/map"
 
 type ResultMapController = IController & {
     center: AppSettings["map_center"]
@@ -21,73 +34,12 @@ type ResultMapScope = IScope & {
     showMap: boolean
 }
 
-export type MarkerGroup = {
-    selected: boolean
-    order: number
-    color: string
-    markers: Record<string, Marker>
-}
-
-export type Marker = MarkerEvent & {
-    lat: number
-    lng: number
-    label: string
-}
-
-export type MarkerEvent = {
-    point: Point
-    queryData: MarkerQueryData
-}
-
-/** Needed for making a sub-search */
-export type MarkerQueryData = {
-    searchCqp: string
-    subCqp: string
-    label: string
-    corpora: string[]
-    within?: string
-}
-
-type CustomMarker = L.Marker & {
-    markerData: MarkerData
-}
-
-type CustomMarkerMany = L.Marker & {
-    markerData: MarkerData[]
-}
-
-type MarkerData = MarkerEvent & {
-    label: string
-    color: string
-}
-
-type MergedMarker = {
-    markerData: MarkerData[]
-    lat: number
-    lng: number
-}
-
 type MessageScope = IScope & {
     showLabel?: boolean
     point?: MarkerData["point"]
     label?: string
     color?: string
 }
-
-class MarkerClusterGroup extends L.MarkerClusterGroup {
-    getAllChildMarkers: () => CustomMarker[]
-}
-
-class MarkerCluster extends L.MarkerCluster {
-    getAllChildMarkers: () => CustomMarker[]
-}
-
-/** Determine if a given layer is a single marker */
-const isMarker = <T extends L.Layer>(layer: T | CustomMarker): layer is CustomMarker => "markerData" in layer
-
-/** Determine if a given layer is a cluster marker */
-const isMarkerCluster = <T extends L.Layer>(layer: T | MarkerClusterGroup): layer is MarkerClusterGroup =>
-    "getChildCount" in layer
 
 angular.module("korpApp").component("resultMap", {
     template: html`<div class="map">
@@ -160,96 +112,10 @@ angular.module("korpApp").component("resultMap", {
 
             $scope.$on("update_map", () => $timeout(() => map.invalidateSize()))
 
-            function createCircleMarker(color: string, diameter: number, borderRadius: number) {
-                return L.divIcon({
-                    html: html`<div
-                        class="geokorp-marker"
-                        style="border-radius:${borderRadius}px; height:${diameter}px; background-color:${color}"
-                    ></div>`,
-                    iconSize: new L.Point(diameter, diameter),
-                })
-            }
-
             function createMarkerIcon(color: string, cluster: boolean) {
                 // TODO use scope.maxRel, but scope.maxRel is not set when markers are created
                 // diameter = ((relSize / scope.maxRel) * 45) + 5
                 return createCircleMarker(color, 10, cluster ? 1 : 5)
-            }
-
-            /**
-             * Organizes several markers at the same location in a 2D grid.
-             * Like:
-             *    o
-             *   oOo
-             */
-            function createMultiMarkerIcon(markerData: MarkerData[]) {
-                const elements = markerData.map((marker) => {
-                    const diameter = (marker.point.rel / maxRel) * 40 + 10
-                    return [
-                        diameter,
-                        html`<div
-                            class="geokorp-multi-marker"
-                            style="border-radius:${diameter}px; height:${diameter}px; width:${diameter}px; background-color:${marker.color}"
-                        ></div>`,
-                    ] as [number, string]
-                })
-
-                elements.sort((a, b) => a[0] - b[0])
-
-                const gridSizeRaw = Math.ceil(Math.sqrt(elements.length)) + 1
-                const gridSize = gridSizeRaw % 2 === 0 ? gridSizeRaw + 1 : gridSizeRaw
-                const center = Math.floor(gridSize / 2)
-
-                const gridRaw: ([number, string][] | [])[] = []
-                for (let i = 0; i <= gridSize - 1; i++) gridRaw.push([])
-
-                const id = (x: number) => x
-                const neg = (x: number) => -x
-                for (let idx = 0; idx <= center; idx++) {
-                    let x = -1
-                    let y = -1
-                    let xOp = neg
-                    let yOp = neg
-                    const stop = idx === 0 ? 0 : idx * 4 - 1
-                    for (let j = 0; j <= stop; ++j) {
-                        x = x === -1 ? center + idx : x + xOp(1)
-                        y = y === -1 ? center : y + yOp(1)
-                        if (x === center - idx) xOp = id
-                        if (y === center - idx) yOp = id
-                        if (x === center + idx) xOp = neg
-                        if (y === center + idx) yOp = neg
-                        const circle = elements.pop()
-                        if (circle) {
-                            gridRaw[y][x] = circle
-                        } else {
-                            break
-                        }
-                    }
-                }
-                // remove all empty arrays and elements
-                // TODO don't create empty stuff??
-                const grid = gridRaw.filter((row) => row.length > 0).map((row) => row.filter((elem) => elem))
-
-                //# take largest element from each row and add to height
-                let height = 0
-                let width = 0
-                const gridCenter = Math.floor(grid.length / 2)
-
-                const rows = grid.map((row, idx) => {
-                    height += row.reduce((memo, val) => (val[0] > memo ? val[0] : memo), 0)
-                    if (idx === gridCenter) {
-                        width = grid[gridCenter].reduce((memo, val) => memo + val[0], 0)
-                    }
-                    const markerClass =
-                        idx === gridCenter ? "marker-middle" : idx > gridCenter ? "marker-top" : "marker-bottom"
-                    return html`<div class="${markerClass}" style="text-align:center; line-height:0;">
-                        ${row.map((elem) => elem[1]).join("")}
-                    </div>`
-                })
-                return L.divIcon({
-                    html: rows.join(""),
-                    iconSize: new L.Point(width, height),
-                })
             }
 
             /**
@@ -469,7 +335,7 @@ angular.module("korpApp").component("resultMap", {
                     const markers = $ctrl.selectedGroups.map((group) => $ctrl.markers[group])
                     const markersMerged = mergeMarkers(markers)
                     for (const markerData of markersMerged) {
-                        const icon = createMultiMarkerIcon(markerData.markerData)
+                        const icon = createMultiMarkerIcon(markerData.markerData, maxRel)
                         const marker = L.marker([markerData.lat, markerData.lng], { icon }) as CustomMarkerMany
                         marker.markerData = markerData.markerData
                         featureLayer.addLayer(marker)
