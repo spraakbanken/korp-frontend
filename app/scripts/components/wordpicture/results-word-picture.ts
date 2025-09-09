1f54866f6885fb88687837ab1063f7534ec3b927
Arild Matsson <arild.matsson@gu.se>
2025-09-09 12:28:08 +0200

    refactor: move up wordpic search options to results component

--- app/scripts/components/wordpicture/results-word-picture.ts
+++ app/scripts/components/wordpicture/results-word-picture.ts
@@ -1,9 +1,9 @@
 import angular, { IController, IScope, ITimeoutService } from "angular"
 import { RelationsProxy, RelationsEmptyError, TableDrawData, RelationsQuery } from "@/backend/proxy/relations-proxy"
 import { html } from "@/util"
-import { RootScope } from "@/root-scope.types"
 import { RelationsSort } from "@/backend/types/relations"
 import { loc } from "@/i18n"
+import "@/components/util/help-box"
 import "@/components/util/json_button"
 import "@/components/util/korp-error"
 import "@/components/wordpicture/word-picture"
@@ -16,23 +16,63 @@ type ResultsWordPictureController = IController & {
 }
 
 type ResultsWordPictureScope = IScope & {
-    $root: RootScope
     activated: boolean
-    changeSort: (sort: RelationsSort) => void
     data?: TableDrawData[]
     error?: string
+    limit: string // Number as string to work with <select ng-model>
+    limitOptions: number[]
     proxy: RelationsProxy
+    showWordClass: boolean
     /** Sort param for the relations request. */
     sort: RelationsSort
+    /** Model for the sort input. */
+    sortLocal: RelationsSort
     warning?: string
 }
 
+const LIMITS: readonly number[] = [15, 50, 100, 500, 1000]
+
 angular.module("korpApp").component("resultsWordPicture", {
     template: html`
-        <div ng-if="!error">
-            <word-picture data="data" on-sort-change="changeSort(sort)" sort="sort" warning="warning"></word-picture>
-        </div>
+        <div ng-if="warning" class="korp-warning" role="status">{{warning}}</div>
         <korp-error ng-if="error" message="{{error}}"></korp-error>
+
+        <div ng-show="!error && data.length">
+            <div class="flex flex-wrap items-baseline mb-4 gap-4 bg-gray-100 p-2">
+                <label>
+                    <input ng-model="showWordClass" type="checkbox" />
+                    {{'show_wordclass' | loc:$root.lang}}
+                </label>
+                <select ng-model="limit">
+                    <option ng-repeat="option in limitOptions" value="{{option}}">
+                        {{'word_pic_show_some' | loc:$root.lang}} {{option}} {{'word_pic_hits' | loc:$root.lang}}
+                    </option>
+                </select>
+                <div class="flex flex-wrap gap-2">
+                    {{'sort_by' | loc:$root.lang}}:
+                    <label>
+                        <input type="radio" value="mi" ng-model="sortLocal" />
+                        {{'stat_lmi' | loc:$root.lang}}
+                        <i
+                            class="fa fa-info-circle text-gray-400 table-cell align-middle mb-0.5"
+                            uib-tooltip="{{'stat_lmi_help' | loc:$root.lang}}"
+                        ></i>
+                    </label>
+                    <label>
+                        <input type="radio" value="freq" ng-model="sortLocal" />
+                        {{'stat_frequency' | loc:$root.lang}}
+                    </label>
+                </div>
+            </div>
+
+            <word-picture data="data" limit="limit" show-word-class="showWordClass" sort="sort"></word-picture>
+        </div>
+
+        <help-box>
+            <p>{{'word_pic_description' | loc:$root.lang}}</p>
+            <p>{{'word_pic_result_description' | loc:$root.lang}}</p>
+        </help-box>
+
         <json-button ng-if="!warning && !error" endpoint="relations" data="proxy.response"></json-button>
     `,
     bindings: {
@@ -46,9 +86,33 @@ angular.module("korpApp").component("resultsWordPicture", {
         "store",
         function ($scope: ResultsWordPictureScope, $timeout: ITimeoutService, store: StoreService) {
             const $ctrl = this as ResultsWordPictureController
-            $scope.proxy = new RelationsProxy()
             $scope.activated = false
+            $scope.limit = String(LIMITS[0])
+            $scope.limitOptions = [...LIMITS]
+            $scope.proxy = new RelationsProxy()
             $scope.sort = "mi"
+            $scope.sortLocal = "mi"
+
+            $scope.$watch("data", () => {
+                if (!$scope.data) return
+                // Find length of longest column
+                const max = Math.max(
+                    ...$scope.data.flatMap((word) =>
+                        word.data.flatMap((table) =>
+                            table.map((col) => (Array.isArray(col.table) ? col.table.length : 0)),
+                        ),
+                    ),
+                )
+                // Include options up to the first that is higher than the longest column
+                const endIndex = LIMITS.findIndex((limit) => limit >= max)
+                $scope.limitOptions = LIMITS.slice(0, endIndex + 1)
+                // Clamp previously selected value
+                if (Number($scope.limit) > LIMITS[endIndex]) $scope.limit = String(LIMITS[endIndex])
+                // Apply sort
+                $scope.sort = $scope.sortLocal
+            })
+
+            $scope.$watch("sortLocal", () => $scope.sortLocal && makeRequest())
 
             store.watch("globalFilter", () => {
                 if (store.globalFilter) $scope.warning = loc("word_pic_global_filter", store.lang)
@@ -74,11 +138,6 @@ angular.module("korpApp").component("resultsWordPicture", {
                     $ctrl.setProgress(false, 0)
                 }
             })
-
-            $scope.changeSort = (sort) => {
-                $scope.sort = sort
-                makeRequest()
-            }
 
             function resetView(warning?: string) {
                 $scope.data = undefined
@@ -107,7 +166,7 @@ angular.module("korpApp").component("resultsWordPicture", {
                 $scope.warning = undefined
                 $scope.proxy
                     .setProgressHandler((progressObj) => $timeout(() => $ctrl.setProgress(true, progressObj.percent)))
-                    .makeRequest(query.type, query.word, $scope.sort)
+                    .makeRequest(query.type, query.word, $scope.sortLocal)
                     .then((data) => $timeout(() => ($scope.data = data)))
                     .catch((error) => {
                         // AbortError is expected if a new search is made before the previous one is finished
--- app/scripts/components/wordpicture/word-picture.ts
+++ app/scripts/components/wordpicture/word-picture.ts
@@ -1,137 +1,60 @@
-import angular, { IController } from "angular"
+import angular, { IController, IScope } from "angular"
 import { html } from "@/util"
 import { TableDrawData } from "@/backend/proxy/relations-proxy"
 import { RelationsSort } from "@/backend/types/relations"
-import "@/components/util/help-box"
 import "./word-picture-section"
 import { Lemgram } from "@/lemgram"
 
 type WordPictureController = IController & {
-    // Bindings
     data: TableDrawData[]
-    onSortChange: (args: { sort: RelationsSort }) => void
-    sort: RelationsSort
-    warning?: string
-
-    // Locals
-    limit: string // Number as string to work with <select ng-model>
-    limitOptions: number[]
+    limit: number
     showWordClass: boolean
-    sortLocal: RelationsSort
-    statProp: RelationsSort
+    sort: RelationsSort
+}
+
+type WordPictureScope = IScope & {
     isLemgram: (word: string) => boolean
     lemgramToHtml: (word: string) => string
 }
 
-const LIMITS: readonly number[] = [15, 50, 100, 500, 1000]
-
 angular.module("korpApp").component("wordPicture", {
     template: html`
-        <div ng-if="$ctrl.warning" class="korp-warning" role="status">{{$ctrl.warning}}</div>
+        <div class="content_target flex flex-wrap gap-4 items-start">
+            <section class="radialBkg p-2 border border-gray-400" ng-repeat="word in $ctrl.data">
+                <h2 class="text-xl mb-4">
+                    <span ng-if="isLemgram(word.token)" ng-bind-html="lemgramToHtml(word.token) | trust"></span>
+                    <span ng-if="!isLemgram(word.token)">
+                        {{word.token}} ({{word.wordClassShort | loc:$root.lang}})
+                    </span>
+                </h2>
 
-        <div ng-if="$ctrl.data.length">
-            <div class="flex flex-wrap items-baseline mb-4 gap-4 bg-gray-100 p-2">
-                <label>
-                    <input ng-model="$ctrl.showWordClass" type="checkbox" />
-                    {{'show_wordclass' | loc:$root.lang}}
-                </label>
-                <select ng-model="$ctrl.limit">
-                    <option ng-repeat="option in $ctrl.limitOptions" value="{{option}}">
-                        {{'word_pic_show_some' | loc:$root.lang}} {{option}} {{'word_pic_hits' | loc:$root.lang}}
-                    </option>
-                </select>
-                <div class="flex flex-wrap gap-2">
-                    {{'sort_by' | loc:$root.lang}}:
-                    <label>
-                        <input type="radio" value="mi" ng-model="$ctrl.sortLocal" ng-change="$ctrl.changeSort()" />
-                        {{'stat_lmi' | loc:$root.lang}}
-                        <i
-                            class="fa fa-info-circle text-gray-400 table-cell align-middle mb-0.5"
-                            uib-tooltip="{{'stat_lmi_help' | loc:$root.lang}}"
-                        ></i>
-                    </label>
-                    <label>
-                        <input type="radio" value="freq" ng-model="$ctrl.sortLocal" ng-change="$ctrl.changeSort()" />
-                        {{'stat_frequency' | loc:$root.lang}}
-                    </label>
-                </div>
-            </div>
-
-            <div class="content_target flex flex-wrap gap-4 items-start">
-                <section class="radialBkg p-2 border border-gray-400" ng-repeat="word in $ctrl.data">
-                    <h2 class="text-xl mb-4">
-                        <span
-                            ng-if="$ctrl.isLemgram(word.token)"
-                            ng-bind-html="$ctrl.lemgramToHtml(word.token) | trust"
-                        ></span>
-                        <span ng-if="!$ctrl.isLemgram(word.token)">
-                            {{word.token}} ({{word.wordClassShort | loc:$root.lang}})
-                        </span>
-                    </h2>
-
-                    <word-picture-section
-                        ng-repeat="section in word.data"
-                        limit="$ctrl.limit"
-                        parent-index="$index"
-                        section="section"
-                        show-word-class="$ctrl.showWordClass"
-                        sort="$ctrl.statProp"
-                        token="word.token"
-                        word-class="word.wordClass"
-                    >
-                    </word-picture-section>
-                </section>
-            </div>
+                <word-picture-section
+                    ng-repeat="section in word.data"
+                    limit="$ctrl.limit"
+                    parent-index="$index"
+                    section="section"
+                    show-word-class="$ctrl.showWordClass"
+                    sort="$ctrl.sort"
+                    token="word.token"
+                    word-class="word.wordClass"
+                >
+                </word-picture-section>
+            </section>
         </div>
-
-        <help-box>
-            <p>{{'word_pic_description' | loc:$root.lang}}</p>
-            <p>{{'word_pic_result_description' | loc:$root.lang}}</p>
-        </help-box>
     `,
     bindings: {
         data: "<",
-        onSortChange: "&",
+        limit: "<",
+        showWordClass: "<",
         sort: "<",
-        warning: "<",
     },
     controller: [
-        function () {
+        "$scope",
+        function ($scope: WordPictureScope) {
             const $ctrl = this as WordPictureController
 
-            $ctrl.limitOptions = [...LIMITS]
-            $ctrl.limit = String(LIMITS[0])
-            $ctrl.showWordClass = false
-
-            $ctrl.$onChanges = (changes) => {
-                if ("data" in changes && changes.data.currentValue) {
-                    $ctrl.statProp = $ctrl.sort
-                    // Find length of longest column
-                    const max = Math.max(
-                        ...$ctrl.data.flatMap((word) =>
-                            word.data.flatMap((table) =>
-                                table.flatMap((col) => (Array.isArray(col.table) ? col.table.length : 0)),
-                            ),
-                        ),
-                    )
-                    // Include options up to the first that is higher than the longest column
-                    const endIndex = LIMITS.findIndex((limit) => limit >= max)
-                    $ctrl.limitOptions = LIMITS.slice(0, endIndex + 1)
-                    // Clamp previously selected value
-                    if (Number($ctrl.limit) > LIMITS[endIndex]) $ctrl.limit = String(LIMITS[endIndex])
-                }
-
-                if ("sort" in changes) {
-                    $ctrl.sortLocal = changes.sort.currentValue
-                }
-            }
-
-            $ctrl.changeSort = () => {
-                $ctrl.onSortChange({ sort: $ctrl.sortLocal })
-            }
-
-            $ctrl.isLemgram = (id) => !!Lemgram.parse(id)
-            $ctrl.lemgramToHtml = (id) => Lemgram.parse(id)!.toHtml()
+            $scope.isLemgram = (id) => !!Lemgram.parse(id)
+            $scope.lemgramToHtml = (id) => Lemgram.parse(id)!.toHtml()
         },
     ],
 })
