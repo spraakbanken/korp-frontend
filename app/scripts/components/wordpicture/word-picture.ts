726fc0faf50e60ae54f4775124d1db12e0a3cacf
Arild Matsson <arild.matsson@gu.se>
2025-09-26 16:30:29 +0200

    Select segment

--- app/scripts/components/wordpicture/word-picture.ts
+++ app/scripts/components/wordpicture/word-picture.ts
@@ -16,10 +16,32 @@ type WordPictureScope = IScope & {
     fromLemgram: (word: string) => string
     isLemgram: (word: string) => boolean
     lemgramToHtml: (word: string) => string
+    segmentIndex: number
 }
 
 angular.module("korpApp").component("wordPicture", {
     template: html`
+        <div ng-show="$ctrl.data.segments.length > 1" class="my-2">
+            {{'word_pic_segment' | loc:$root.lang}}: {{$ctrl.data.segments[segmentIndex]}}
+
+            <div class="w-full max-w-screen-sm">
+                <input
+                    type="range"
+                    max="{{$ctrl.data.segments.length - 1}}"
+                    list="wordpic-trend-markers"
+                    ng-model="segmentIndex"
+                    class="w-full"
+                />
+                <datalist id="wordpic-trend-markers">
+                    <option
+                        ng-repeat="segment in $ctrl.data.segments"
+                        value="{{$index}}"
+                        label="{{ segment }}"
+                    ></option>
+                </datalist>
+            </div>
+        </div>
+
         <div class="content_target flex flex-wrap gap-4 items-start">
             <section class="radialBkg p-2 border border-gray-400" ng-repeat="section in $ctrl.data.getData()">
                 <h2 class="text-xl mb-4">
@@ -47,7 +69,7 @@ angular.module("korpApp").component("wordPicture", {
                         css-class="column.config.css_class"
                         items="column.rows"
                         limit="$ctrl.limit"
-                        segment="$ctrl.data.segments[0]"
+                        segment="$ctrl.data.segments[segmentIndex]"
                         show-word-class="$ctrl.showWordClass"
                         sort="$ctrl.sort"
                     ></word-picture-column>
@@ -65,6 +87,13 @@ angular.module("korpApp").component("wordPicture", {
         "$scope",
         function ($scope: WordPictureScope) {
             const $ctrl = this as WordPictureController
+
+            $ctrl.$onChanges = (changes) => {
+                if (changes.data?.currentValue) {
+                    // Set to most last segment (most recent timespan)
+                    $scope.segmentIndex = $ctrl.data.segments.length - 1
+                }
+            }
 
             $scope.fromLemgram = (word) => Lemgram.parse(word)?.form || word
             $scope.isLemgram = (id) => !!Lemgram.parse(id)
