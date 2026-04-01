eaee4c6f3b4d6defa13b7f41e1df06319d753b6a
Arild Matsson <arild.matsson@gu.se>
2026-04-01 09:51:55 +0200

    refactor: remove msdtags.html
    The content was moved to https://spraakbanken.gu.se/verktyg/korp/msdtags
    Configure a redirect and update links!

--- webpack.common.js
+++ webpack.common.js
@@ -148,10 +148,6 @@ module.exports = {
                     to: "translations/[name].[fullhash][ext]",
                 },
                 {
-                    from: "app/markup/msdtags.html",
-                    to: "markup",
-                },
-                {
                     from: "app/translations/locale-*.json",
                     to: "translations/[name].[fullhash][ext]",
                 },
